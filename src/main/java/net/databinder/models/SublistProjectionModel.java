6b3daf0a1e08bb06a733c1987fa7711cb72db976
Nathan Hamblen <nathan@technically.us>
2006-07-19 20:27:54 +0000

    fix bug in sublist project calc
    git-svn-id: svn://databinder.net/databinder/trunk@472 c322e9cc-cc08-0410-988e-c197163ad9eb

--- src/main/java/net/databinder/models/SublistProjectionModel.java
+++ src/main/java/net/databinder/models/SublistProjectionModel.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 import wicket.Component;
 import wicket.WicketRuntimeException;
-import wicket.model.AbstractDetachableModel;
 import wicket.model.IModel;
+import wicket.model.LoadableDetachableModel;
 
 /**
  * Projects a single list into a multiple, arbitrarily transformed sublists without replicating
@@ -16,141 +16,129 @@ import wicket.model.IModel;
  *  
  * @author Nathan Hamblen
  */
-public abstract class SublistProjectionModel extends AbstractDetachableModel
+public abstract class SublistProjectionModel extends LoadableDetachableModel
 {
-	/** Continuous list used to feed this model's sublists. */
-	private IModel master;
-	
-	private transient List<List> parent;
+        /** Continuous list used to feed this model's sublists. */
+        private IModel master;
+        
+        public SublistProjectionModel(IModel master)
+        {
+                this.master = master;
+        }
 
-	public SublistProjectionModel(IModel master)
-	{
-		this.master = master;
-	}
+        /** @return number of sublists */
+        protected abstract int getParentSize();
 
-	/** @return number of sublists */
-	protected abstract int getParentSize();
+        /** @return index of master list mapped to parameters */
+        protected abstract int transform(int parentIdx, int sublistIdx);
 
-	/** @return index of master list mapped to parameters */
-	protected abstract int transform(int parentIdx, int sublistIdx);
+        /** @return size sublist at given index*/
+        protected abstract int getSize(int parentIdx);
 
-	/** @return size sublist at given index*/
-	protected abstract int getSize(int parentIdx);
+        /**
+         * Breaks the parent list into chunks of the requested size, in the same order as
+         * the parent list.
+         */
+        public static class Chunked extends SublistProjectionModel {
 
-	/**
-	 * Breaks the parent list into chunks of the requested size, in the same order as
-	 * the parent list.
-	 */
-	public static class Chunked extends SublistProjectionModel {
+                protected int chunkSize;
 
-		protected int chunkSize;
+                public Chunked(int chunkSize, IModel master)
+                {
+                        super(master);
+                        this.chunkSize = chunkSize;
+                }
 
-		public Chunked(int chunkSize, IModel master)
-		{
-			super(master);
-			this.chunkSize = chunkSize;
-		}
+                protected int transform(int parentIdx, int sublistIdx)
+                {
+                        return parentIdx * chunkSize + sublistIdx;
+                }
 
-		protected int transform(int parentIdx, int sublistIdx)
-		{
-			return parentIdx * chunkSize + sublistIdx;
-		}
+                protected int getSize(int parentIdx) {
+                        return Math.min(getMasterList().size() - parentIdx * chunkSize, chunkSize);
+                }
 
-		protected int getSize(int parentIdx) {
-			return Math.min(getMasterList().size() - parentIdx * chunkSize, chunkSize);
-		}
+                protected int getParentSize()
+                {
+                        return (getMasterList().size() - 1) / chunkSize + 1;
+                }
+        }
 
-		protected int getParentSize()
-		{
-			return (getMasterList().size() - 1) / chunkSize + 1;
-		}
-	}
+        /**
+         * Transposes rows and columns so the list runs top to bottom rather than
+         * left to right. 
+         */
+        public static class Transposed extends Chunked  {
 
-	/**
-	 * Transposes rows and columns so the list runs top to bottom rather than
-	 * left to right. 
-	 */
-	public static class Transposed extends Chunked  {
+                public Transposed(int columns, IModel master)
+                {
+                        super(columns, master);
+                }
 
-		public Transposed(int columns, IModel master)
-		{
-			super(columns, master);
-		}
+                protected int transform(int parentIdx, int sublistIdx)
+                {
+                        return parentIdx + sublistIdx * getParentSize();
+                }
 
-		protected int transform(int parentIdx, int sublistIdx)
-		{
-			return parentIdx + sublistIdx * getParentSize();
-		}
-	}
+                protected int getSize(int parentIdx) {
+                    int lastColLen = getMasterList().size() % getParentSize();;
+                    if (lastColLen == 0 || lastColLen > parentIdx)
+                        return chunkSize;
+                    return chunkSize - 1;
+                }
 
-	protected List getMasterList()
-	{
-		return (List)master.getObject(null);
-	}
+        }
 
-	/**
-	 * Instantiates parent list, made up entirely of projected sublists.
-	 */
-	@Override
-	protected Object onGetObject(Component component)
-	{
-		if (parent == null) {
-			int rows = getParentSize();
-			parent = new ArrayList<List>(rows);
-			for (int i = 0; i < rows; i++)
-				parent.add(new ProjectedSublist(i));
-		}
-		return parent;
-	}
+        protected List<List> getMasterList()
+        {
+                return (List<List>)master.getObject(null);
+        }
 
-	@Override
-	public void onSetObject(Component component, Object object)
-	{
-		throw new WicketRuntimeException("This model is read only.");
-	}
+        @Override
+        protected List<List>load() {
+                int rows = getParentSize();
+                List<List> parent = new ArrayList<List>(rows);
+                for (int i = 0; i < rows; i++)
+                        parent.add(new ProjectedSublist(i));
 
-	/**
-	 * This is a virtual list, a projection of the master list. Its size and index trasform is 
-	 * governed by the containing object.
-	 */
-	@SuppressWarnings("unchecked")
-	protected class ProjectedSublist extends AbstractList
-	{
-		private int parentIdx;
+                return parent;
+        }
 
-		public ProjectedSublist(final int parentIdx)
-		{
-			this.parentIdx = parentIdx;
-		}
+        /**
+         * This is a virtual list, a projection of the master list. Its size and index trasform is 
+         * governed by the containing object.
+         */
+        @SuppressWarnings("unchecked")
+        protected class ProjectedSublist extends AbstractList
+        {
+                private int parentIdx;
 
-		@Override
-		public Object get(final int index)
-		{
-			return getMasterList().get(transform(parentIdx, index));
-		}
+                public ProjectedSublist(final int parentIdx)
+                {
+                        this.parentIdx = parentIdx;
+                }
 
-		@Override
-		public int size()
-		{
-			return getSize(parentIdx);
-		}
-	}
+                @Override
+                public Object get(final int index)
+                {
+                        return getMasterList().get(transform(parentIdx, index));
+                }
 
-	@Override
-	public void onDetach()
-	{
-		master.detach();
-		parent = null;
-	}
+                @Override
+                public int size()
+                {
+                        return getSize(parentIdx);
+                }
+        }
 
-	@Override
-	public IModel getNestedModel()
-	{
-		return null;
-	}
+        @Override
+        public IModel getNestedModel()
+        {
+                return null;
+        }
 
-	@Override
-	protected void onAttach()
-	{
-	}
+        @Override
+        protected void onAttach()
+        {
+        }
 }
