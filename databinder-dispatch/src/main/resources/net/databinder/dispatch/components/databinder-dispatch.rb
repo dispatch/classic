a19bbda091f803b43ba58f0a4ee44b626909b248
Nathan Hamblen <nathan@technically.us>
2007-01-15 05:21:45 +0000

    dispatch setup
    git-svn-id: svn://databinder.net/databinder/trunk@1055 c322e9cc-cc08-0410-988e-c197163ad9eb

---
+++ databinder-dispatch/src/main/resources/net/databinder/dispatch/components/databinder-dispatch.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+# or otherwise obtain redcloth
+require 'xmlrpc/server'
+require 'redcloth'
+
+s = XMLRPC::Server.new(8180)
+# listens on localhost only
+# If needed extertnally, pass in IP address as second parameter
+
+s.add_handler("redcloth.to_html") do |input|
+  r = RedCloth.new input
+  r.to_html
+end
+
+s.set_default_handler do |name, *args|
+  raise XMLRPC::FaultException.new(-99, "Method #{name} missing" +
+                                   " or wrong number of parameters!")
+end
+
+s.serve
