42c427aa8b2ae361df5da75f13b197e2b1367b62
Nathan Hamblen <nathan@technically.us>
2007-01-20 18:14:28 +0000

    markdown, rubypants support
    git-svn-id: svn://databinder.net/databinder/trunk@1073 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-dispatch/src/main/resources/net/databinder/dispatch/components/databinder-dispatch.rb
+++ databinder-dispatch/src/main/resources/net/databinder/dispatch/components/databinder-dispatch.rb
@@ -5,7 +5,6 @@ require 'webrick'
 require 'xmlrpc/server'
 
 require 'rubygems' # or otherwise obtain redcloth
-require 'redcloth'
 
 WEBrick::Daemon.start # forks process
 
@@ -14,13 +13,29 @@ s = XMLRPC::Server.new(8180)
 # If needed extertnally, pass in IP address as second parameter
 
 s.add_handler("redcloth.to_html") do |input|
-  r = RedCloth.new input
-  r.to_html
+	require 'redcloth'
+  RedCloth.new(input).to_html
+end
+
+s.add_handler("bluecloth.to_html") do |input|
+	require 'bluecloth'
+  BlueCloth.new(input).to_html
+end
+
+s.add_handler("rubypants.to_html") do |input|
+	require 'rubypants'
+  RubyPants.new(input).to_html
+end
+
+s.add_handler("bluecloth.rubypants.to_html") do |input|
+	require 'bluecloth'
+	require 'rubypants'
+  RubyPants.new(BlueCloth.new(input).to_html).to_html
 end
 
 s.set_default_handler do |name, *args|
   raise XMLRPC::FaultException.new(-99, "Method #{name} missing" +
-                                   " or wrong number of parameters!")
+                                   " or wrong number of parameters")
 end
 
 s.serve
