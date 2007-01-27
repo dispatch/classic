be8778d0231fa984fe72c00f1bba7557096c0410
Nathan Hamblen <nathan@technically.us>
2007-01-27 20:11:17 +0000

    repating view for iterable entities
    git-svn-id: svn://databinder.net/databinder/trunk@1088 c322e9cc-cc08-0410-988e-c197163ad9eb

---
+++ databinder/src/main/java/net/databinder/components/IterableEntityView.java
@@ -0,0 +1,92 @@
+/*
+ * Databinder: a simple bridge from Wicket to Hibernate
+ * Copyright (C) 2006  Nathan Hamblen nathan@technically.us
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package net.databinder.components;
+
+import java.util.Iterator;
+
+import net.databinder.models.HibernateObjectModel;
+import wicket.markup.repeater.RefreshingView;
+import wicket.model.BoundCompoundPropertyModel;
+import wicket.model.IModel;
+
+/**
+ * Similar to a PropertyListView, but works with any Iterable collection composed of
+ * persisted entities. This is particularly useful for the common Hibernate set mapping, whose 
+ * contents would otherwise need to be placed in a new List after loading. Contained items
+ * must be Hibernate entities; a Hibernate exeception will be thrown otherwise. 
+ * @author Nathan Hamblen
+ */
+public abstract class IterableEntityView extends RefreshingView {
+	
+	/**
+	 * Contruct with externally bound model whose object must be Iterable.
+	 * @param id
+	 */
+	public IterableEntityView(String id) {
+		super(id);
+	}
+	
+	/**
+	 * @param id Wicket id
+	 * @param model Wrapped object must be Iterable.
+	 */
+	public IterableEntityView(String id, IModel model) {
+		super(id, model);
+	}	
+	
+	@Override
+	protected final Iterator getItemModels() {
+		return new ModelIterator((Iterable)getModelObject());
+	}
+	
+	/**
+	 * Wraps o in a HibernateObjectModel inside a BoundCompoundPropertyModel. Override
+	 * if the compound property model is not desired.
+	 * @param o object to be wrapped
+	 * @return detachable model wrapping object
+	 */
+	protected IModel model(Object o) {
+		return new BoundCompoundPropertyModel(new HibernateObjectModel(o));
+	}
+	
+	private class ModelIterator implements Iterator
+	{
+		private Iterator iterator;
+
+		public ModelIterator(Iterable items)
+		{
+			this.iterator = items.iterator();
+		}
+
+		public void remove()
+		{
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean hasNext()
+		{
+			return iterator.hasNext();
+		}
+
+		public Object next()
+		{
+			return model(iterator.next());
+		}
+	}
+}
