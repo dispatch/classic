0377999eb37554358ec64c897d174c686af02bf3
Nathan Hamblen <nathan@technically.us>
2007-01-27 18:11:03 +0000

    don't extend DataPage
    git-svn-id: svn://databinder.net/databinder/trunk@1084 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder/src/main/java/net/databinder/conv/components/DataConversationPage.java
+++ databinder/src/main/java/net/databinder/conv/components/DataConversationPage.java
@@ -1,10 +1,10 @@
 package net.databinder.conv.components;
 
-import net.databinder.components.DataPage;
-
 import org.hibernate.classic.Session;
 
-public abstract class DataConversationPage extends DataPage implements IConversationPage {
+import wicket.markup.html.WebPage;
+
+public abstract class DataConversationPage extends WebPage implements IConversationPage {
 
 	private Session conversationSession;
 	
