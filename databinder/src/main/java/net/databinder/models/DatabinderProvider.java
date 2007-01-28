0ead3806b800589fa344bc4306187329523b64e4
Nathan Hamblen <nathan@technically.us>
2007-01-28 05:47:43 +0000

    DatabinderProvider more like HibernateListModel
    git-svn-id: svn://databinder.net/databinder/trunk@1092 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder/src/main/java/net/databinder/models/DatabinderProvider.java
+++ databinder/src/main/java/net/databinder/models/DatabinderProvider.java
@@ -28,140 +28,230 @@ import org.hibernate.Session;
 import org.hibernate.criterion.Projections;
 
 import wicket.markup.repeater.data.IDataProvider;
+import wicket.model.BoundCompoundPropertyModel;
+import wicket.model.IModel;
 
 /**
- * Provides base functionality for DataView IDataProvider. Because Databinder does not link
- * to wicket-extensions, this class does not declare its implementation of IDataProvider.
- * However, it implements all methods of that interface and should make a task-specific
- * IDataProvider implementation considerably easier to build. Internal subclasses
- * are providers for both standard and criteria queries.
- * @see wicket.extensions.markup.html.repeater.data.IDataProvider  
+ * Provides query results to DataView and related components. Like the Hibernate model classes,
+ * the results of this provider can be altered by query binders and criteria builders. By default
+ * this provider wraps items in a compound property model in addition to a Hibernate model.
+ * This is convenient for mapping DataView subcomponents as bean properties (as with
+ * PropertyListView). However, <b>DataTable will not work with a compound property model.</b>
+ * Call setWrapWithPropertyModel(false) when using with DataTable, DataGridView, or any
+ * other time you do not want a compound property model.
  * @author Nathan Hamblen
  */
-public abstract class DatabinderProvider implements IDataProvider  {
+public class DatabinderProvider implements IDataProvider  {
+	private Class objectClass;
+	private ICriteriaBuilder criteriaBuilder, sortCriteriaBuilder;
+	private String queryString, countQueryString;
+	private IQueryBinder queryBinder, countQueryBinder;
+	/** Controls wrapping with a compound property model. */
+	private boolean wrapWithPropertyModel = true;
+	
 	/**
-	 * @param object object DataView would like to wrap
-	 * @return object wrapped in a HibernateObjectModel
+	 * Provides all entities of the given class.
 	 */
-	public HibernateObjectModel model(Object object) {
-		return new HibernateObjectModel(object);
+	public DatabinderProvider(Class objectClass) {
+		this.objectClass = objectClass;
 	}
 	
 	/**
-	 * A data provider controlled by the Hibernate Criteria API. Subclass must provide the
-	 * implementation for ICriteriaBuilder.
+	 * Provides entities of the given class meeting the supplied criteria.
+	 */
+	public DatabinderProvider(Class objectClass, ICriteriaBuilder criteriaBuilder) {
+		this(objectClass);
+		this.criteriaBuilder = criteriaBuilder;
+	}
+
+	/**
+	 * Provides entities of the given class meeting the supplied critiera and sort criteria. The
+	 * sort criteria is not applied to the count query (as it should not affect the count). When returning
+	 * results it is applied in addition to the standard criteria, so it is not necessary to duplicate
+	 * filter or other criteria in the sort criteria.
+	 * @param criteriaBuilder standard criteria applied to both the count and actual results
+	 * @param sortCriteriaBuilder sort criteria applied only to the actual results
+	 */
+	public DatabinderProvider(Class objectClass, ICriteriaBuilder criteriaBuilder, ICriteriaBuilder sortCriteriaBuilder) {
+		this(objectClass, criteriaBuilder);
+		this.sortCriteriaBuilder = sortCriteriaBuilder;
+	}
+	
+	/**
+	 * Provides entities matching the given query. The count query
+	 * is derived by prefixing "select count(*)" to the given query; this will fail if 
+	 * the supplied query has a select clause.
+	 */
+	public DatabinderProvider(String query) {
+		this.queryString = query;
+	}
+	
+	/**
+	 * Provides entities matching the given query with bound parameters.  The count query
+	 * is derived by prefixing "select count(*)" to the given query; this will fail if 
+	 * the supplied query has a select clause.
+	 */
+	public DatabinderProvider(String query, IQueryBinder queryBinder) {
+		this(query);
+		this.queryBinder = queryBinder;
+	}
+
+	/**
+	 * Provides entities matching the given queries with bound parameters.
+	 * @param query query to return entities
+	 * @param queryBinder binder for the standard query
+	 * @param countQuery query to return count of entities
+	 * @param countQueryBinder binder for the count query
+	 */
+	public DatabinderProvider(String query, IQueryBinder queryBinder, String countQuery, IQueryBinder countQueryBinder) {
+		this(query, queryBinder);
+		this.countQueryString = countQuery;
+		this.countQueryBinder = countQueryBinder;
+	}
+
+	/** Used only by deprecated subclasses. Please ignore.  */
+	protected void setCriteriaBuilder(ICriteriaBuilder criteriaBuilder) {
+		this.criteriaBuilder = criteriaBuilder;
+	}
+
+	/** Used only by deprecated subclasses. Please ignore.  */
+	protected void setSortCriteriaBuilder(ICriteriaBuilder sortCriteriaBuilder) {
+		this.sortCriteriaBuilder = sortCriteriaBuilder;
+	}
+	
+	/** Used only by deprecated subclasses. Please ignore.  */
+	protected void setQueryBinder(IQueryBinder queryBinder) {
+		this.queryBinder = queryBinder;
+	}
+
+	/**
+	 * It should not be necessary to override (or call) this default implementation.
+	 */
+	public final Iterator iterator(int first, int count) {
+		Session sess =  DataStaticService.getHibernateSession();
+		
+		if (queryString != null) {
+			org.hibernate.Query q = sess.createQuery(queryString);
+			if (queryBinder != null)
+				queryBinder.bind(q);
+			q.setFirstResult(first);
+			q.setMaxResults(count);
+			return q.iterate();
+		}
+		
+		Criteria crit = sess.createCriteria(objectClass);
+		if (criteriaBuilder != null) {
+			criteriaBuilder.build(crit);
+			if (sortCriteriaBuilder != null)
+				sortCriteriaBuilder.build(crit);
+		}
+		crit.setFirstResult(first);
+		crit.setMaxResults(count);
+		return crit.list().iterator();
+	}
+	
+	/**
+	 * It should not be necessary to override (or call) this default implementation.
+	 */
+	public final int size() {
+		Session sess =  DataStaticService.getHibernateSession();
+
+		if (countQueryString != null) {
+			Query query = sess.createQuery(countQueryString);
+			if (countQueryBinder != null)
+				countQueryBinder.bind(query);
+			return ((Long) query.uniqueResult()).intValue();
+		}
+		if (queryString != null) {
+			String synthCountQuery = "select count(*) " + queryString;
+			Query query = sess.createQuery(synthCountQuery);
+			if (queryBinder != null)
+				queryBinder.bind(query);
+			return ((Long) query.uniqueResult()).intValue();
+		}
+		
+		Criteria crit = sess.createCriteria(objectClass);
+		
+		if (criteriaBuilder != null)
+			criteriaBuilder.build(crit);
+		crit.setProjection(Projections.rowCount());
+		return (Integer) crit.uniqueResult();
+	}
+
+	/**
+	 * @return true if wrapping items with a compound property model (the default)
+	 */
+	public boolean getWrapWithPropertyModel() {
+		return wrapWithPropertyModel;
+	}
+
+	/**
+	 * @param wrapInCompoundModel false to not wrap items in a compound property model (true is default)
+	 */
+	public void setWrapWithPropertyModel(boolean wrapInCompoundModel) {
+		this.wrapWithPropertyModel = wrapInCompoundModel;
+	}
+
+	/**
+	 * Wraps object in HiberanteObjectModel, and also BoundCompoundPropertyModel if 
+	 * wrapInCompoundModel is true.
+	 * @param object object DataView would like to wrap
+	 * @return object wrapped in a HibernateObjectModel and possibly BoundCompoundPropertyModel
+	 */
+	public IModel model(Object object) {
+		HibernateObjectModel model = new HibernateObjectModel(object);
+		return  wrapWithPropertyModel ? new BoundCompoundPropertyModel(model) : model;
+	}
+	
+	/**
+	 * Please use the base DatabinderProvider with independent criteria builders. This subclass
+	 * will be removed in a future release.
+	 * @deprecated
 	 */
 	public static abstract class CriteriaBuilder extends DatabinderProvider implements ICriteriaBuilder {
-		private Class objectClass;
-		
-		/**
-		 * @param objectClass type of objects returned by this provider
-		 */
 		public CriteriaBuilder(Class objectClass) {
-			this.objectClass = objectClass;
+			super(objectClass);
+			setCriteriaBuilder(this);
+			setSortCriteriaBuilder(new ICriteriaBuilder() {
+				public void build(Criteria criteria) {
+					sort(criteria);
+				}
+			});
+			setWrapWithPropertyModel(false);
 		}
-		
-		/**
-		 * It should not be necessary to override (or call) this default implementation.
-		 *  See the sort() method for sortable providers.
-		 */
-		public final Iterator iterator(int first, int count) {
-			Session sess =  DataStaticService.getHibernateSession();
-			
-			Criteria crit = sess.createCriteria(objectClass);
-			build(crit);
-			sort(crit);
-			crit.setFirstResult(first);
-			crit.setMaxResults(count);
-	
-			return crit.list().iterator();
-		}
-		
-		/**
-		 * It should not be necessary to override (or call) this default implementation.
-		 */
-		public final int size() {
-			Session sess =  DataStaticService.getHibernateSession();
-	
-			Criteria crit = sess.createCriteria(objectClass);
-			build(crit);
-			crit.setProjection(Projections.rowCount());
-			return (Integer) crit.uniqueResult();
-		}
-		
-		/**
-		 * Override this method if your provider is sortable. It will be called when building 
-		 * the actual results (and not the count).
-		 * @param crit criteria to receive ordering
-		 */
 		protected void sort(Criteria crit) {
 		}
 	}
-	
 	/**
-	 * A data provider controlled by a Hibernate query. Subclass must provide the
-	 * implementation for IQueryBinder.
+	 * Please use the base DatabinderProvider with independent query binders. This subclass
+	 * will be removed from a future release.
+	 * @deprecated
 	 */
 	public static abstract class QueryBinder extends DatabinderProvider implements IQueryBinder {
 		String queryString;
 		
-		/**
-		 * @param queryString Base string, including any parameters, for query.
-		 */
 		public QueryBinder(String queryString) {
-			this.queryString = queryString;
+			super(queryString);
+			setQueryBinder(this);
+			setWrapWithPropertyModel(false);
 		}
 		
 		/**
-		 * It should not be necessary to override (or call) this default implementation.
-		 *  See the sort() method for sortable providers.
+		 * This method will not be called. Please use the base DatabinderProvider.
 		 */
-		public final Iterator iterator(int first, int count) {
-			Session sess =  DataStaticService.getHibernateSession();
-			
-			Query query = sess.createQuery(sort(queryString));
-			bind(query);
-			query.setFirstResult(first);
-			query.setMaxResults(count);
-	
-			return query.list().iterator();
+		protected final String count (String queryString) {
+			return null;
 		}
 		
 		/**
-		 * It should not be necessary to override (or call) this default implementation.
+		 * This method will not be called. Please use the base DatabinderProvider.
 		 */
-		public final int size() {
-			Session sess =  DataStaticService.getHibernateSession();
-	
-			Query query = sess.createQuery(count(queryString));
-			bind(query);
-			return ((Long) query.uniqueResult()).intValue();
-		}
-
-		/**
-		 * Modify query to return row count. This implementation prepends <tt>select count(*)</tt>
-		 * to the query, which will work only if the base query string has no select clause. Override
-		 * this method if necessary, and note that the base query string may be ignored if 
-		 * appropriate. 
-		 * @param queryString the base query string for this provider
-		 * @return query stirng returning row count only
-		 */
-		protected String count (String queryString) {
-			return "select count(*) " + queryString;
-		}
-		
-		/**
-		 * Override this method if your provider is sortable. It will be called when building 
-		 * the actual results (and not the count). The base query string is provided for your
-		 * convenience.
-		 * @param queryString the base query string for this provider
-		 * @return query string including any ordering clause
-		 */
-		protected String sort(String queryString) {
-			return queryString;
+		protected final String sort(String queryString) {
+			return null;
 		}
 	}
-	/** This class has nothing to detach. */
+	
+	/** This provider has nothing to detach. */
 	public void detach() {
 		
 	}
