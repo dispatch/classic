ac70b0cd5d418cee89035f15ee5da54e9eb5a8f6
Nathan Hamblen <nathan@technically.us>
2007-07-08 01:20:00 +0000

    sourcelist docs
    git-svn-id: svn://databinder.net/databinder/trunk@1468 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-components/src/main/java/net/databinder/components/SourceList.java
+++ databinder-components/src/main/java/net/databinder/components/SourceList.java
@@ -8,10 +8,26 @@ import java.util.List;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.link.Link;
 
+/**
+ * Generator for links to components (usually panels) that are only visible one at a time.
+ * Similar to TabbedPanel, but the links and target component can be placed anywhere in 
+ * the associated markup template. Ex:
+ * <pre>
+ * SourceList sl = new SourceList();
+ * add(sl.new SourceLink("link-a", new MyPanel("panel-a")));
+ * add(sl.new SourceLink("link-b", new MyOtherPanel("panel-b")));
+ * </pre>
+ * @author Nathan Hamblen
+ */
 public class SourceList implements Serializable {
 	List<SourceLink> links = new LinkedList<SourceLink>();
 	SourceLink current;
 	
+	/** 
+	 * Link that that causes the paired component to be visible when clicked, and all other
+	 * components in the SourceList to be invisible. Paired components are set to invisible
+	 * on initialization; to make select a linked component programmatically, call onClick().  
+	 */
 	public class SourceLink extends Link {
 		Component target;
 		public SourceLink(String id, Component target) {
@@ -20,6 +36,7 @@ public class SourceList implements Serializable {
 			links.add(this);
 			target.setVisible(false);
 		}
+		/** return false when paired component is selected */
 		@Override
 		public boolean isEnabled() {
 			return current != this;
