54be72228c4ed5d1fe025eedda427c34b4ac3964
Nathan Hamblen <nathan@technically.us>
2007-07-09 00:29:20 +0000

    profile updates
    git-svn-id: svn://databinder.net/databinder/trunk@1472 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-auth-components/src/main/java/net/databinder/auth/components/DataProfilePage.java
+++ databinder-auth-components/src/main/java/net/databinder/auth/components/DataProfilePage.java
@@ -1,12 +1,37 @@
+/*
+ * Databinder: a simple bridge from Wicket to Hibernate
+ * Copyright (C) 2006  Nathan Hamblen nathan@technically.us
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.databinder.auth.components;
 
 import net.databinder.components.DataStyleLink;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebPage;
 
+/**
+ * Display profile editing panel for logged in user. 
+ */
 public class DataProfilePage extends WebPage {
 	public DataProfilePage() {
 		add(new DataStyleLink("dataStylesheet"));
-		add(new DataProfilePanel("registerPanel"));
+		add(profileSocket("profileSocket"));
+	}
+	protected Component profileSocket(String id) {
+		return new DataProfilePanel(id);
 	}
 }
