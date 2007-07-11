05a8a3469a29a7df8438b21e02699e8e163c8bed
Nathan Hamblen <nathan@technically.us>
2007-07-11 02:22:30 +0000

    entity filter
    git-svn-id: svn://databinder.net/databinder/trunk@1509 c322e9cc-cc08-0410-988e-c197163ad9eb

---
+++ databinder-components/src/main/java/net/databinder/components/EntityFilteredPropertyColumn.java
@@ -0,0 +1,35 @@
+package net.databinder.components;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.ChoiceFilter;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.ChoiceFilteredPropertyColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+
+public class EntityFilteredPropertyColumn extends ChoiceFilteredPropertyColumn {
+	private ChoiceRenderer choiceRenderer;
+	private String displayProperty;
+
+	public EntityFilteredPropertyColumn(IModel displayModel, String sortProperty, String displayProperty, String propertyExpression, String filterLabelProperty, IModel filterChoices) {
+		super(displayModel,sortProperty,propertyExpression,filterChoices);
+		choiceRenderer = new ChoiceRenderer(filterLabelProperty);
+		this.displayProperty = displayProperty;
+	}
+
+	protected IChoiceRenderer getChoiceRenderer() {
+		return choiceRenderer;
+	}
+
+	protected IModel createLabelModel(IModel embeddedModel) {
+		return new PropertyModel(embeddedModel, displayProperty);
+	}
+
+	public Component getFilter(String componentId, FilterForm form) {
+		ChoiceFilter cf = (ChoiceFilter) super.getFilter(componentId, form);
+		cf.getChoice().setNullValid(true);
+		return cf;
+	}
+}
