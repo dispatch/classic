277a3142eba801882d337dc52cd1a2296ad7c552
Nathan Hamblen <nathan@technically.us>
2007-08-01 02:28:35 +0000

    docs
    git-svn-id: svn://databinder.net/databinder/trunk@1586 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-components/src/main/java/net/databinder/components/hibernate/PageSourceListPanel.java
+++ databinder-components/src/main/java/net/databinder/components/hibernate/PageSourceListPanel.java
@@ -6,15 +6,36 @@ import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
 
+/**
+ * Displays a list of PageSourceLinks to the selected page. The panel renders to an
+ * unordered list of class <tt>source-list</tt>.
+ * @see PageSourceLink
+ * @author Nathan Hamblen
+ */
 public class PageSourceListPanel extends SourceListPanel {
 	private Class<? extends Page> pageClass;
 	private String idParameter;
 
+	/**
+	 * Creates list with identifiers bound to an "id" parameter.
+	 * @param id component id
+	 * @param page targeted page
+	 * @param bodyProperty object property for link body text
+	 * @param listModel list of entities to render
+	 */
 	public PageSourceListPanel(String id, Class<? extends Page> page, String bodyProperty, IModel listModel ) {
 		super(id, bodyProperty, listModel);
 		this.pageClass = page;
 	}
 
+	/**
+	 * Creates list with identifiers bound to the chosen parameter
+	 * @param id component id
+	 * @param page targeted page
+	 * @param bodyProperty object property for link body text
+	 * @param idParameter identifer passed through this parameter
+	 * @param listModel list of entities to render
+	 */
 	public PageSourceListPanel(String id, Class<? extends Page> page, 
 			String bodyProperty, String idParameter, IModel listModel ) {
 		super(id, bodyProperty, listModel);
@@ -22,6 +43,7 @@ public class PageSourceListPanel extends SourceListPanel {
 		this.idParameter = idParameter;
 	}
 
+	/** Constructs source links. */
 	@Override
 	protected Link sourceLink(String id, final IModel model) {
 		PageSourceLink link = new PageSourceLink(id, pageClass, model, idParameter) {
