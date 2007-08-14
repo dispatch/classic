028bf491326d309c2954f1bb070df4e1687ab3dd
Nathan Hamblen <nathan@technically.us>
2007-08-14 01:54:35 +0000

    docs
    git-svn-id: svn://databinder.net/databinder/trunk@1661 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-models/src/main/java/net/databinder/IDataApplication.java
+++ databinder-models/src/main/java/net/databinder/IDataApplication.java
@@ -2,6 +2,18 @@ package net.databinder;
 
 import org.hibernate.SessionFactory;
 
+/**
+ * Databinder application interface. DataStaticService expects the current Wicket
+ * application to conform to this interface and supply a session factory as needed.
+ * @see DataStaticService
+ * @author Nathan Hamblen
+ */
 public interface IDataApplication {
+	/**
+	 * Supply the session factory for the given key. Applications needing only one
+	 * session factory may return it without inspecting the key parameter.
+	 * @param factory key, or null for the default factory
+	 * @return configured Hibernate session factory
+	 */
 	SessionFactory getHibernateSessionFactory(Object key);
 }
