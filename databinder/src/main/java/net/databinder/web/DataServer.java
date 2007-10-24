7c5528d40ea93e9a3935358194964fec4f7a0744
Nathan Hamblen <nathan@technically.us>
2007-10-24 12:48:36 +0000

    docs
    git-svn-id: svn://databinder.net/databinder/trunk@1785 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder/src/main/java/net/databinder/web/DataServer.java
+++ databinder/src/main/java/net/databinder/web/DataServer.java
@@ -6,6 +6,14 @@ import org.mortbay.jetty.Server;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 
+/**
+ * Optional main() class for running embedded Jetty. Client applications may pass
+ * this classname to the Java runtime to serve with no other configuration. The webroot 
+ * is expected to be in src/main/webapp, and the server defaults to a root servlet context 
+ * on port 8080. <tt>jetty.contextPath</tt> and <tt>jetty.port</tt> system properties
+ * may be used to override (e.g. <tt>-Djetty.port=80</tt> as a command line parameter).
+ * @author Nathan Hamblen
+ */
 public class DataServer {
 
 	public static void main(String[] args) throws Exception
