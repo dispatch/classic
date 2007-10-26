33a153513ba64456824070469f0c404e9c4acd86
Nathan Hamblen <nathan@technically.us>
2007-10-26 03:40:51 +0000

    logging and docs
    git-svn-id: svn://databinder.net/databinder/trunk@1799 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder/src/main/java/net/databinder/web/DataServer.java
+++ databinder/src/main/java/net/databinder/web/DataServer.java
@@ -20,25 +20,25 @@ package net.databinder.web;
 
 import java.io.File;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.util.string.Strings;
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.MovedContextHandler;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Optional main() class for running embedded Jetty. Client applications may pass
  * this classname to the Java runtime to serve with no other configuration. The webroot 
- * defaults to src/main/webapp, and the server to a root servlet context 
+ * defaults to src/main/webapp, and the server to a context named after the current directory 
  * on port 8080. <tt>jetty.warPath</tt>, <tt>jetty.contextPath</tt>, and <tt>jetty.port</tt> system properties
  * may be used to override (e.g. <tt>-Djetty.port=80</tt> as a command line parameter).
  * @author Nathan Hamblen
  */
 public class DataServer {
-	private static final Log log = LogFactory.getLog(DataServer.class);
+	private static final Logger log = LoggerFactory.getLogger(DataServer.class);
 
 	public static void main(String[] args) throws Exception
 	{
@@ -47,12 +47,17 @@ public class DataServer {
 		WebAppContext web = new WebAppContext();
 		
 		String contextPath = System.getProperty("jetty.contextPath");
-		if (Strings.isEmpty(contextPath)) 
+		if (Strings.isEmpty(contextPath)) {
 			contextPath = "/" + new File(".").getCanonicalFile().getName();
+			log.info("context path by current directory: " + contextPath);
+		}
+		else
+			log.info("jetty.contextPath property: " + contextPath);
 		web.setContextPath(contextPath);
 		
 		String warPath = System.getProperty("jetty.warPath");
 		if (Strings.isEmpty(warPath)) warPath = "src/main/webapp";
+		else log.info("jetty.warPath property: " + warPath);
 		web.setWar(warPath);
 		
 		server.addHandler(web);
@@ -63,6 +68,7 @@ public class DataServer {
 		SelectChannelConnector connector = new SelectChannelConnector();
 		try {
 			connector.setPort(Integer.valueOf(System.getProperty("jetty.port")));
+			log.info("jetty.port property: " + connector.getPort());
 		} catch (NumberFormatException e) {
 			connector.setPort(8080);
 		}
