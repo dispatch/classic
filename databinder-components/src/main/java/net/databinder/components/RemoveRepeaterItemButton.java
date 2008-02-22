4f7fd4456ad205adfdb28ea2432a42276bbfc279
Thomas Kappler <tkappler@googlemail.com>
2008-02-22 09:47:40 +0000

    Support a minimum number of elements in the collection.  Feel free to remove if too obscure/specific.
    git-svn-id: svn://databinder.net/databinder/branches/r1.1.1@2208 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-components/src/main/java/net/databinder/components/RemoveRepeaterItemButton.java
+++ databinder-components/src/main/java/net/databinder/components/RemoveRepeaterItemButton.java
@@ -5,19 +5,34 @@ import java.util.Collection;
 import org.apache.wicket.markup.repeater.Item;
 
 public class RemoveRepeaterItemButton extends RepeaterItemButton {
+	
+	@Override
+	public boolean isEnabled() {
+		Collection<?> c = (Collection<?>) getView().getModelObject();
+		return c.size() > minElements;
+	}
+
+	private Integer minElements = 0;
+
+	public RemoveRepeaterItemButton(String id, Item item) {
+		super(id, item, getTrashImage());
+	}
+	
+	public RemoveRepeaterItemButton(String id, Item item, Integer minElements) {
+		this(id, item);
+		this.minElements = minElements;
+	}
    
-   public RemoveRepeaterItemButton(String id, Item item) {
-      super(id, item, getTrashImage());
-   }
-   
-   @Override
-   public void onSubmit() {
-      Collection c = (Collection) getView().getModelObject();
-      if (c != null) {
-         getView().modelChanging();
-         // depends on correct equals()!
-         c.remove(item.getModelObject());
-         getView().modelChanged();
-      }
-   }
+	@Override
+	public void onSubmit() {
+		Collection c = (Collection) getView().getModelObject();
+		if (c != null) {
+			if (c.size() > minElements) {
+				getView().modelChanging();
+				// depends on correct equals()!
+				c.remove(item.getModelObject());
+				getView().modelChanged();
+			}
+		}
+	}
 }
