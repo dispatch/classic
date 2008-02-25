e56e3c1ad9e2e623dabf4202224f3aca8ce43ecc
Nathan Hamblen <nathan@technically.us>
2008-02-25 01:35:53 +0000

    exception filtering
    git-svn-id: svn://databinder.net/databinder/trunk@2218 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-app/src/main/java/net/databinder/ExceptionFilteringRequestCycle.java
+++ databinder-app/src/main/java/net/databinder/ExceptionFilteringRequestCycle.java
@@ -1,3 +1,21 @@
+/*
+ * Databinder: a simple bridge from Wicket to Hibernate
+ * Copyright (C) 2008  Nathan Hamblen nathan@technically.us
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.databinder;
 
 import java.util.regex.Pattern;
@@ -9,15 +27,24 @@ import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Request cycle that logs runtime exceptions as warnings if their origin matches
+ * a pattern.
+ * @author Nathan Hamblen
+ */
 public abstract class ExceptionFilteringRequestCycle extends WebRequestCycle {
 	
 	private static final Logger log = LoggerFactory.getLogger(ExceptionFilteringRequestCycle.class);
+	/** Default pattern is ".*UrlCodingStrategy" */
 	private static Pattern warnOnlySource = Pattern.compile(".*UrlCodingStrategy");
 	
 	public ExceptionFilteringRequestCycle(WebApplication application, WebRequest request, Response response) {
 		super(application, request, response);
 	}
 	
+	/**
+	 * Logs runtime exception as warning if it matches the warnOnlySource pattern.
+	 */
 	@Override
 	protected void logRuntimeException(RuntimeException e) {
 		if (warnOnlySource.matcher(e.getStackTrace()[0].getClassName()).matches())
@@ -26,6 +53,10 @@ public abstract class ExceptionFilteringRequestCycle extends WebRequestCycle {
 			super.logRuntimeException(e);
 	}
 	
+	/**
+	 * Change the warn testing pattern used by all instances of this class.
+	 * @param pattern to match against fully qualified class name of exception origin
+	 */
 	public static void setWarnOnlySource(Pattern pattern) {
 		warnOnlySource = pattern;
 	}
