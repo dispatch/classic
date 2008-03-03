b9b74ae280b8f66c35b34b70ffb97ef8bb0ea0be
Nathan Hamblen <nathan@technically.us>
2008-03-03 21:47:59 +0000

    dispatch daemon
    git-svn-id: svn://databinder.net/databinder/trunk@2297 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-dispatch/src/main/resources/net/databinder/dispatch/components/databinder-dispatch.rb
+++ databinder-dispatch/src/main/resources/net/databinder/dispatch/components/databinder-dispatch.rb
@@ -6,27 +6,82 @@ include WEBrick
 
 require 'rubygems' # or otherwise obtain redcloth, etc.
 
-WEBrick::Daemon.start # forks process
+PID = '/var/run/databinder-dispatch.pid'
 
-s = HTTPServer.new( :Port => 8180 )
-# listens on localhost only
-# If needed extertnally, pass in IP address as second parameter
-
-def input(req) req.query['input'] end
-
-s.mount_proc('/redcloth') do |req, resp|
-	require 'redcloth'
-	resp.body = RedCloth.new(input(req)).to_html
+def pid(new_id = false)
+  if new_id
+    File.open(PID, 'w') do |f| f << new_id end
+  else 
+    File.exist?(PID) && IO.read(PID).to_i
+  end
 end
 
-s.mount_proc('/maruku') do |req, resp|
-	require 'maruku'
-	resp.body = Maruku.new(input(req)).to_html
+def start()
+  puts "Warning, existing pid record: " + pid.to_s if pid
+  pid(-1)         # test that we can write before forking
+  WEBrick::Daemon.start # forks process
+  pid(Process.pid)
+  run
 end
 
-s.mount_proc('/rubypants') do |req, resp|
-	require 'rubypants'
-	resp.body = RubyPants.new(input(req)).to_html
+def stop()
+  if pid
+    begin
+      Process.kill("INT", pid)
+    rescue Errno::ESRCH
+      puts "server not running at pid: #{pid}; removing record"
+    end
+    File.unlink PID
+  else
+    puts "no server id on record"
+  end
 end
 
-s.start
+def run()
+  s = HTTPServer.new( :Port => 8180 )
+  # listens on localhost only
+  # If needed extertnally, pass in IP address as second parameter
+
+  def input(req) req.query['input'] end
+
+  s.mount_proc('/redcloth') do |req, resp|
+  	require 'redcloth'
+  	resp.body = RedCloth.new(input(req)).to_html
+  end
+
+  s.mount_proc('/maruku') do |req, resp|
+  	require 'maruku'
+  	resp.body = Maruku.new(input(req)).to_html
+  end
+
+  s.mount_proc('/rubypants') do |req, resp|
+  	require 'rubypants'
+  	resp.body = RubyPants.new(input(req)).to_html
+  end
+  
+  trap("INT") { s.shutdown }
+  s.start
+end
+
+begin
+  case !ARGV.empty? && ARGV[0]
+  when 'run'
+    run
+  when 'start'
+    start
+  when 'stop'
+    stop
+  when 'restart'
+    stop
+    start
+  else
+    puts "Invalid command. Please specify run, start, stop or restart."
+    exit
+  end
+rescue Errno::EACCES
+  puts "Error, unable to write: " + PID
+  exit(1)
+rescue Errno::EPERM
+  puts "Error, not permitted to kill pid: " + pid.to_s
+  exit(1)
+end
