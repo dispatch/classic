97a4c1efd6f11ead5f61feb82da5be911587f21d
Nathan Hamblen <nathan@technically.us>
2008-03-11 23:44:11 +0000

    rake
    git-svn-id: svn://databinder.net/databinder/trunk@2326 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-scouch/scratch.scala
+++ databinder-scouch/scratch.scala
@@ -3,8 +3,14 @@ import scala.util.parsing.json._
 import org.apache.commons.httpclient._
 import org.apache.commons.httpclient.methods._
 
-val cl = new HttpClient
-cl.getHostConfiguration.setHost("localhost", 8888)
-def ex(m: HttpMethod) = { println(cl executeMethod m); m getResponseBodyAsString }
+class RichHttpClient(host: String, port: int) extends HttpClient {
+  getHostConfiguration.setHost(host, port)
+  
+  def ? (m: HttpMethod)(fail: (Int)=>HttpMethod) = executeMethod(m) match {
+      case 200 => m
+      case code => fail(code)
+  }
+  def ! (m: HttpMethod) = (this ? m)(code => error("Response not OK: " + code))
+}
 
-
+val couch = new RichHttpClient("localhost", 5984)
