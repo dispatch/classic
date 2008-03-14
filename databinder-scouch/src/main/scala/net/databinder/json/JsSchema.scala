9a88cbdd6591e8a0308c294d54eac56248b3158b
Nathan Hamblen <nathan@technically.us>
2008-03-14 22:45:55 +0000

    could recurse, but no def access
    git-svn-id: svn://databinder.net/databinder/trunk@2336 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-scouch/src/main/scala/net/databinder/json/JsSchema.scala
+++ databinder-scouch/src/main/scala/net/databinder/json/JsSchema.scala
@@ -2,24 +2,42 @@ package net.databinder.json
 
 import java.io.InputStream
 
-trait JsSchema {
-  def string(sym: Symbol) = JsString(sym)
-//  def obj(sym: Symbo)
+trait JsObject extends JsValue[Map[String, Any]] {
+  def my(sym: Symbol) = new {
+    def as(value: JsValue[_]) = {
+      value.sym = Some(sym)
+      value.parent = Some(JsObject.this)
+      value
+    }
+  }
+  def loc(base: Map[String, Option[Any]], sub_sym: Symbol): Option[Map[String, Option[Any]]] = 
+    ((parent match {
+        case Some(parent) => parent.loc(base, sym.get)
+        case None => Some(base)
+      }) flatMap { _(sub_sym.name) }
+    ).asInstanceOf[Option[Map[String, Option[Any]]]]
 }
 
-case class JsString(sym: Symbol) extends JsValue[String](sym)  
-case class JsObject(sym: Symbol) extends JsValue[Map[String, Any]](sym)
+class JsString extends JsValue[String]
 
-class JsValue[T](val symbol: Symbol)
+trait JsValue[T] {
+  var sym: Option[Symbol] = None
+  var parent: Option[JsObject] = None
+  
+  def loc(base: Map[String, Option[Any]]): Option[T] = 
+    (parent flatMap { _.loc(base, sym.get) }).asInstanceOf[Option[T]]
+}
 
-class JsStore (val base: Map[String, Any]){
+
+
+class JsStore (val base: Map[String, Option[Any]]){
   def this(stream: InputStream) = this(Json parse stream)
 
-  def apply[T](ref: JsValue[T]) = base(ref.symbol.name).asInstanceOf[Option[T]]
+  def apply[T](ref: JsValue[T]) = (ref loc base)
   
-  def << [T](ref: JsValue[T])(value: Any): JsStore = 
-    new JsStore(base + (ref.symbol.name -> (value match {
+/*  def << [T](ref: JsValue[T])(value: Any): JsStore = 
+    new JsStore(base + (ref.sym.name -> (value match {
       case opt: Option[_] => opt
       case value => Some(value)
-    })))
+    }))) */
 }
