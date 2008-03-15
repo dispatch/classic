86145f11abf727644a0bef362ed03c86474165c6
Nathan Hamblen <nathan@technically.us>
2008-03-15 17:53:28 +0000

    renames
    git-svn-id: svn://databinder.net/databinder/trunk@2340 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-scouch/src/main/scala/net/databinder/json/JsSchema.scala
+++ databinder-scouch/src/main/scala/net/databinder/json/JsSchema.scala
@@ -1,9 +1,6 @@
 package net.databinder.json
 
-import java.io.InputStream
-
-
-trait JsonSchema {
+trait Schema {
   def loc(base: Option[Map[Symbol, Option[Any]]], sub_sym: Symbol) = 
     (base flatMap { _(sub_sym) }).asInstanceOf[Option[Map[Symbol, Option[Any]]]]
 
@@ -13,27 +10,58 @@ trait JsonSchema {
 
   case class List[T](symbol: Symbol) extends Value[scala.List[T]](symbol)
 
-  case class Object(symbol: Symbol) extends Value[Map[Symbol, Option[Any]]](symbol) with JsonSchema {
+  case class Object(symbol: Symbol) extends Value[Map[Symbol, Option[Any]]](symbol) with Schema {
     override def loc(base: Option[Map[Symbol, Option[Any]]], sub_sym: Symbol) = 
       super.loc(loc(base), sub_sym)
   }
 
   class Value[T](val sym: Symbol) {
     def loc(base: Option[Map[Symbol, Option[Any]]]) = 
-      JsonSchema.this.loc(base, sym).asInstanceOf[Option[T]]
+      Schema.this.loc(base, sym).asInstanceOf[Option[T]]
   }
 }
 
+import scala.util.parsing.json.Parser
+import scala.util.parsing.input.{Reader,StreamReader}
+import java.io.{InputStream, InputStreamReader}
 
+class Store (val base: Map[Symbol, Option[Any]]){
+  def this(stream: InputStream) = this(Store process stream)
 
-class JsStore (val base: Map[Symbol, Option[Any]]){
-  def this(stream: InputStream) = this(Json parse stream)
-
-  def apply[T](ref: JsonSchema#Value[T]) = (ref loc Some(base))
+  def apply[T](ref: Schema#Value[T]) = (ref loc Some(base))
   
-/*  def << [T](ref: JsValue[T])(value: Any): JsStore = 
-    new JsStore(base + (ref.sym.name -> (value match {
+/*  def << [T](ref: JsValue[T])(value: Any): Store = 
+    new Store(base + (ref.sym.name -> (value match {
       case opt: Option[_] => opt
       case value => Some(value)
     }))) */
+}
+
+object Store extends Parser {
+  def process(input: InputStream) = 
+    phrase(root)(new lexical.Scanner(StreamReader(new InputStreamReader(input)))) match {
+      case Success(list: List[_], _) => mapify(list head, list tail)
+      case _ => Map[Symbol, Option[Any]]()
+    }
+
+  private def mapify(tup: Any, list: List[Any]): Map[Symbol, Option[Any]] =
+    (list match {
+      case Nil => Map[Symbol, Option[Any]]()
+      case _ => mapify(list head, list tail)
+    }) + (tup match { case (key: String, value) => Symbol(key) -> resolve(value) })
+
+  def listify(value: Any, list: List[Any]): List[Any] = 
+    resolve(value) :: (list match {
+      case Nil => Nil
+      case list => listify(list head, list tail)
+    })
+
+  def resolve(value: Any) = value match {
+    case list: List[_] => Some(list.head match {
+      case tup: (_, _) => mapify(tup, (list tail))
+      case value => listify(value, list tail)
+    })
+    case null => None
+    case value => Some(value)
+  }
 }
