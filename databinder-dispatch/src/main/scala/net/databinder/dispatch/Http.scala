9eb4370214f1cbb84a02d5096092ab8b7af700ee
Nathan Hamblen <nathan@technically.us>
2008-03-17 05:38:41 +0000

    use Http for HttpLabel
    git-svn-id: svn://databinder.net/databinder/trunk@2346 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-dispatch/src/main/scala/net/databinder/dispatch/Http.scala
+++ databinder-dispatch/src/main/scala/net/databinder/dispatch/Http.scala
@@ -8,33 +8,44 @@ import org.apache.commons.httpclient.methods._
 class Http(host: String, port: int) extends HttpClient {
   getHostConfiguration.setHost(host, port)
   
-  def ? [T] (m: HttpMethod)(thunk: (Int) => T) = try {
+  def exec [T] (m: HttpMethod)(thunk: (Int) => T) = try {
     thunk(executeMethod(m))
   } finally { m.releaseConnection() }
-  
-  def ?! [T] (m: HttpMethod)(check: (Int) => Boolean)(okay: (InputStream) => T) = 
-    (this ? m) { code =>
-      if (check(code))
-        okay(m.getResponseBodyAsStream())
+
+  def exec_if [T] (m: HttpMethod)(chk: (Int) => Boolean)(ok: (HttpMethod) => T) = 
+    (this exec m) { code =>
+      if (chk(code))
+        ok(m)
       else
         error("Response not OK: " + code) }
   
-  def ! [T] (m: HttpMethod)(okay: (InputStream) => T) = 
-    (this ?! m){ code => code >= 200 && code < 300 }(okay)
+  def exec_if_ok [T] (m: HttpMethod)(ok: (HttpMethod) => T) = 
+    (this exec_if m){ code => code >= 200 && code < 300 }(ok)
   
-  def apply(uri: String) = new Action(uri)
+  def apply(uri: String) = new Request(uri)
   
-  class Action(uri: String) {
-    def >> [T] (okay: (InputStream) => T) = 
-      (Http.this ! new GetMethod(uri))(okay)
+  class Request(uri: String) {
+    def >> [T] (ok: (InputStream) => T) =
+      (Http.this exec_if_ok new GetMethod(uri))(m => ok(m.getResponseBodyAsStream))
 
-    def << [T] (body: T) = new {
-      def >> (okay: (InputStream) => T) = {
-        val m = new PutMethod(uri)
-        m setRequestEntity new StringRequestEntity(body.toString)
-        (Http.this ! m)(okay)
-      }
+    def >> = (Http.this exec_if_ok new GetMethod(uri))(m => m.getResponseBodyAsString)
+    
+    def << [T] (body: T) = {
+      val m = new PutMethod(uri)
+      m setRequestEntity new StringRequestEntity(body.toString)
+      new Response(m)
     }
     
+    def << [T] (values: Tuple2[String, Any]*) = {
+      val m = new PostMethod(uri)
+      values foreach { tup => m.setParameter(tup._1, tup._2.toString) }
+      m.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+      new Response(m)
+    }
+    class Response(m: HttpMethod) {
+      def >> [T] (ok: (InputStream) => T) = (Http.this exec_if_ok m)(m => ok(m.getResponseBodyAsStream))
+      def as_str = (Http.this exec_if_ok m)(m => m.getResponseBodyAsString)
+    }
   }
+
 }
--- databinder-dispatch/src/main/scala/net/databinder/dispatch/components/HttpConverter.scala
+++ databinder-dispatch/src/main/scala/net/databinder/dispatch/components/HttpConverter.scala
@@ -6,8 +6,10 @@ import org.apache.wicket.util.convert.IConverter
 import org.apache.wicket.util.convert.converters.AbstractConverter
 import org.apache.wicket.markup.html.basic.Label
 import org.apache.wicket.model.IModel 
-
+import org.apache.wicket.util.io.Streams
 import org.apache.wicket._
+
+import org.apache.commons.httpclient.methods.PostMethod
 
 import org.slf4j.LoggerFactory
 
@@ -15,12 +17,11 @@ import net.sf.ehcache.CacheManager
 import net.sf.ehcache.Ehcache
 import net.sf.ehcache.Element
 
-import org.apache.commons.httpclient._
-import org.apache.commons.httpclient.methods._
+import net.databinder.dispatch.Http
 
 abstract class HttpPostConverter extends AbstractConverter {
 
-  def server_base = "http://localhost:8180/"
+  def service = new Http("localhost", 8180)
   def path_name: String
   
   def convertToObject(value: String, locale: Locale): Object = null
@@ -30,22 +31,10 @@ abstract class HttpPostConverter extends AbstractConverter {
   override def convertToString(source: Object, locale: Locale) = {
     val key = source.hashCode()
     val cache = HttpPostConverter.cache_for(path_name)
-    val elem = cache.get(key)
     
-    if (elem != null) 
-      elem.getValue().asInstanceOf[String]
-    else {
-      val post_method = new PostMethod(server_base + path_name)
-      try {
-        post_method.setParameter("input", source.toString())
-        post_method.setRequestHeader(
-         "Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
-      
-        (new HttpClient).executeMethod(post_method) match {
-          case 200 => ()
-          case code => error("Bad response code: " + code)
-        }
-        val out = post_method.getResponseBodyAsString()
+    cache.get(key) match {
+      case null => try {
+        val out = (service("/" + path_name) << "input" -> source).as_str
         cache.put(new Element(key, out))
         out
       } catch {
@@ -56,9 +45,8 @@ abstract class HttpPostConverter extends AbstractConverter {
             HttpPostConverter.log.error("Error posting to server", e)
             ""
           }
-      } finally {
-        post_method.releaseConnection()
       }
+      case elem => elem.getValue.toString
     }
   }
 }
