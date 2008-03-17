649d4b5629caaf64a0ed9d97f75415b5fae96336
Nathan Hamblen <nathan@technically.us>
2008-03-17 16:58:02 +0000

    ever nicer http interface
    git-svn-id: svn://databinder.net/databinder/trunk@2353 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-dispatch/src/main/scala/net/databinder/dispatch/Http.scala
+++ databinder-dispatch/src/main/scala/net/databinder/dispatch/Http.scala
@@ -9,20 +9,19 @@ class Http(host: String, port: int) extends HttpClient {
   getHostConfiguration.setHost(host, port)
   
   def exec [T] (m: HttpMethod) = new {
-    def apply(thunk: (Int) => T) = try {
-      thunk(executeMethod(m))
+    def apply(thunk: (Int, HttpMethod) => T) = try {
+      thunk(executeMethod(m), m)
     } finally { m.releaseConnection() }
 
-    def when (chk: (Int) => Boolean)(thunk: (HttpMethod) => T) = 
-      this { code =>
+    def when (chk: (Int) => Boolean)(thunk: HttpMethod => T) = 
+      this { (code, m) =>
         if (chk(code))
           thunk(m)
         else
           error("Response not OK: " + code)
       }
-    
-    def ok (thunk: HttpMethod => T) = 
-        (this when { code => code >= 200 && code < 300 })(thunk)
+      
+    def ok = (this when { code => code >= 200 && code < 300 }) _
   }
   def apply(uri: String) = new Request(uri)
   
@@ -37,15 +36,14 @@ class Http(host: String, port: int) extends HttpClient {
       m setRequestEntity new StringRequestEntity(body.toString)
       new Response(m)
     }
-    
-    def << [T] (values: Tuple2[String, Any]*) = {
+    def << [T] (values: (String, Any)*) = {
       val m = new PostMethod(uri)
       values foreach { tup => m.setParameter(tup._1, tup._2.toString) }
       m.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
       new Response(m)
     }
     class Response(m: HttpMethod) {
-      def >> [T] (thunk: (InputStream) => T) = (Http.this exec m) ok (m => thunk(m.getResponseBodyAsStream))
+      def >> [T] (thunk: InputStream => T) = exec(m) ok (m => thunk(m.getResponseBodyAsStream))
       def as_str = exec(m) ok (m => m.getResponseBodyAsString)
     }
   }
