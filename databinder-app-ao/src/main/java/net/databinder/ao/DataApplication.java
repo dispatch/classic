c32112b815ce51ca293e6f0549dbcaf3db04fb84
Nathan Hamblen <nathan@technically.us>
2008-04-15 01:41:53 +0000

    hook for constructing entity manager
    git-svn-id: svn://databinder.net/databinder/trunk@2459 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-app-ao/src/main/java/net/databinder/ao/DataApplication.java
+++ databinder-app-ao/src/main/java/net/databinder/ao/DataApplication.java
@@ -4,12 +4,12 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import net.databinder.DataApplicationBase;
 import net.java.ao.DatabaseProvider;
 import net.java.ao.EntityManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class DataApplication extends DataApplicationBase implements ActiveObjectsApplication {
 	
@@ -18,12 +18,11 @@ public abstract class DataApplication extends DataApplicationBase implements Act
 	private Map<Object, EntityManager> entityManagers = new HashMap<Object, EntityManager>();
 
 	protected void dataInit() {
-		buildEntityManager(null);
+		initEntityManager(null);
 	}
 	
-	protected void buildEntityManager(Object key) {
-		EntityManager entityManager = new EntityManager(buildDatabaseProvider(key));
-		configureEntityManager(entityManager, key);
+	protected void initEntityManager(Object key) {
+		EntityManager entityManager = buildEntityManager(key, buildDatabaseProvider(key));
 		setEntityManager(key, entityManager);
 		if (isDevelopment()) try {
 			generateSchema(entityManager, key);
@@ -32,15 +31,17 @@ public abstract class DataApplication extends DataApplicationBase implements Act
 		}
 	}
 	
-	protected void generateSchema(EntityManager entityManager, Object key) throws SQLException { }
+	protected abstract DatabaseProvider buildDatabaseProvider(Object key);	
+
+	protected EntityManager buildEntityManager(Object key, DatabaseProvider provider) {
+		return new EntityManager(provider);
+	}
 	
-	protected void configureEntityManager(EntityManager entityManager, Object key) { }
+	protected void generateSchema(EntityManager entityManager, Object key) throws SQLException { }
 	
 	protected void setEntityManager(Object key, EntityManager entityManager) {
 		entityManagers.put(key, entityManager);
 	}
-	
-	protected abstract DatabaseProvider buildDatabaseProvider(Object key);
 	
 	public EntityManager getEntityManager(Object key) {
 		return entityManagers.get(key);
