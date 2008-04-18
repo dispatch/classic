c58c980e91a3e885146b36fe44a839b30981f47c
Nathan Hamblen <nathan@technically.us>
2008-04-18 03:45:34 +0000

    docs
    git-svn-id: svn://databinder.net/databinder/trunk@2471 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-models-hib/src/main/java/net/databinder/models/hib/QueryBinderBuilder.java
+++ databinder-models-hib/src/main/java/net/databinder/models/hib/QueryBinderBuilder.java
@@ -1,8 +1,30 @@
+/*
+ * Databinder: a simple bridge from Wicket to Hibernate
+ * Copyright (C) 2006  Nathan Hamblen nathan@technically.us
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package net.databinder.models.hib;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+/**
+ * Converts a string and QueryBinder(s) into a builder so only builders need to
+ * be dealt with. 
+ */
 public class QueryBinderBuilder implements QueryBuilder {
 	private String query;
 	private QueryBinder[] binders;
