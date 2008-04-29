c3f848fd7eabb5f4c639425c3c68ac6601d8b6d4
Nathan Hamblen <nathan@technically.us>
2008-04-29 18:10:08 +0000

    criteria builders need to be serializable
    git-svn-id: svn://databinder.net/databinder/trunk@2527 c322e9cc-cc08-0410-988e-c197163ad9eb

--- databinder-models-hib/src/main/java/net/databinder/models/hib/OrderingCriteriaBuilder.java
+++ databinder-models-hib/src/main/java/net/databinder/models/hib/OrderingCriteriaBuilder.java
@@ -1,12 +1,14 @@
 package net.databinder.models.hib;
 
+import java.io.Serializable;
+
 import org.hibernate.Criteria;
 
 /**
  * Builds criteria objects with or without order. Only one of the build methods
  * should be called in building a criteria object.
  */
-public interface OrderingCriteriaBuilder {
+public interface OrderingCriteriaBuilder extends Serializable {
 	/** Build the criteria without setting an order */
 	public void buildUnordered(Criteria criteria);
 	/** Build the (entire) criteria, including an order */
