106006ea6146637196aba31d78a8692e65d80db5
Nathan Hamblen <nathan@technically.us>
2008-10-28 12:30:22 -0400

    request c3p0 connection provider

--- databinder-app-hib/src/main/java/net/databinder/hib/DataApplication.java
+++ databinder-app-hib/src/main/java/net/databinder/hib/DataApplication.java
@@ -1,6 +1,6 @@
 /*
  * Databinder: a simple bridge from Wicket to Hibernate
- * Copyright (C) 2006  Nathan Hamblen nathan@technically.us
+ * Copyright (C) 2006	Nathan Hamblen nathan@technically.us
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -9,12 +9,12 @@
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA	02110-1301	USA
  */
 
 package net.databinder.hib;
@@ -89,7 +89,7 @@ public abstract class DataApplication extends DataApplicationBase implements Hib
 	}
 	
 	/**
-	 * Builds and  a session factory with the given configuration. Passes config
+	 * Builds and	a session factory with the given configuration. Passes config
 	 * through configureHibernate methods.
 	 * @param key session factory key; the default key is null
 	 * @param config annotation conifuration
@@ -110,7 +110,7 @@ public abstract class DataApplication extends DataApplicationBase implements Hib
 	 * @param config configuration to update
 	 * @param key object, or null for the default factory
 	 */
-	protected  void configureHibernate(AnnotationConfiguration config, Object key) {
+	protected	void configureHibernate(AnnotationConfiguration config, Object key) {
 		configureHibernate(config);
 	}
 	
@@ -132,14 +132,16 @@ public abstract class DataApplication extends DataApplicationBase implements Hib
 	 * as required. For deployment it is configured for C3P0 connection pooling.
 	 * @param config used to build Hibernate session factory
 	 */
-	protected  void configureHibernate(AnnotationConfiguration config) {
-    	if (isDevelopment())
-    		config.setProperty("hibernate.hbm2ddl.auto", "update");
-    	else {
-    		config.setProperty("hibernate.c3p0.max_size", "20")
-    		.setProperty("hibernate.c3p0.timeout","3000")
-    		.setProperty("hibernate.c3p0.idle_test_period", "300");
-    	}
+	protected	void configureHibernate(AnnotationConfiguration config) {
+			if (isDevelopment())
+				config.setProperty("hibernate.hbm2ddl.auto", "update");
+			else {
+				config
+					.setProperty("hibernate.connection.provider_class", "org.hibernate.connection.C3P0ConnectionProvider")
+					.setProperty("hibernate.c3p0.max_size", "20")
+					.setProperty("hibernate.c3p0.timeout","3000")
+					.setProperty("hibernate.c3p0.idle_test_period", "300");
+			}
 	}
 	
 	/**
