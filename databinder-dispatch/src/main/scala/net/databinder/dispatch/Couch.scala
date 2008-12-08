8f7cfcf7224a8c936dc93181caf8d39b5d17c4ed
Nathan Hamblen <nathan@technically.us>
2008-12-08 08:22:31 -0500

    couch database wrapper

--- databinder-dispatch/src/main/scala/net/databinder/dispatch/Couch.scala
+++ databinder-dispatch/src/main/scala/net/databinder/dispatch/Couch.scala
@@ -1,6 +1,7 @@
 package net.databinder.dispatch
 
 import java.io.InputStream
+import org.apache.http.HttpHost
 
 trait Doc extends Schema {
   val _id = String(Symbol("_id")) 
@@ -8,6 +9,21 @@ trait Doc extends Schema {
 }
 
 object Doc extends Doc
+
+class Database(host: HttpHost, name: String) extends HttpServer(host)  {
+  def this(hostname: String, port: Int, name: String) = this(new HttpHost(hostname, port), name)
+  def this(name: String) = this(new HttpHost("localhost", 5984), name)
+  
+  override def apply(uri: String) = new Request("/"  + name + "/" + uri)
+  def all_docs = (apply("_all_docs") >> { new Store(_) } )(Listing.rows)
+  
+  object Listing extends Schema { 
+    class ListItem(symbol: Symbol) extends Object(symbol) {
+      val id = String('id)
+    }
+    val rows = List[String]('rows)
+  }
+}
 
 object Revise extends Schema {
   val id = String('id)
