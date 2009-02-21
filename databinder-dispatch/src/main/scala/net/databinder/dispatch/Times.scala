5a35a1d4782855aae1615138be1031fbfc5efc9c
Nathan Hamblen <nathan@technically.us>
2009-02-21 18:32:01 -0500

    article search api

--- databinder-dispatch/src/main/scala/net/databinder/dispatch/Times.scala
+++ databinder-dispatch/src/main/scala/net/databinder/dispatch/Times.scala
@@ -7,13 +7,13 @@ trait Times {
   val version: String
   
   def exec(action: String, params: Map[String, Any]) =
-    http("/svc/" + service + "/api/" + version + action) ?< (params + ("api-key" -> api_key))
+    http("/svc/" + service + "/" + version + action) ?< (params + ("api-key" -> api_key))
 
   def exec(action: String): Http#Request = exec(action, Map[String, Any]())
 }
 
 case class People(api_key: String) extends Times {
-  val service = "timespeople";
+  val service = "timespeople/api";
   val version = "v1"
   
   def profile(user_id: Int) = exec("/user/" + user_id + "/profile.js");
