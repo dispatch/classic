ca8fe3d0704799063b17a10716daabdebdb17581
Nathan Hamblen <nathan@technically.us>
2009-03-01 23:22:28 -0500

    rm that reflection stuff

--- databinder-dispatch/scratch.scala
+++ databinder-dispatch/scratch.scala
@@ -1,52 +1,11 @@
-// http://minformix.org/blog/index.php?/archives/91-Scala-Querying-an-objects-fields-and-methods-with-reflection.html
-import scala.Console._  
-import scala.tools.nsc.util.NameTransformer._  
-
-import java.util.regex._
-
-class AnyExtras(x: Any) {  
-  def methods = method_list(ig => true).foreach(println _)  
-  def methods(regex: String) = method_list(has(_, regex)).foreach(println _)
-  def fields = field_list(ig => true).foreach(println _)  
-  def fields(regex: String) = field_list(has(_, regex)).foreach(println _)  
-  
-  def has(str:String, regex: String) = 
-    Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(str).find
-    
-  def method_list(filter: String => Boolean) = wrapped.getClass  
-      .getMethods  
-      .toList
-      .filter(m => filter(m.getName))
-      .map(m => decode(m.toString  
-                        .replaceFirst("\\).*", ")")  
-                        .replaceAll("[^(]+\\.", "")  
-                        .replace("()", "")))  
-      .filter(!_.startsWith("$tag"))  
-      .sort(_ < _)
-    
-  def field_list(filter: String => Boolean) = wrapped.getClass  
-      .getDeclaredFields  
-      .filter(f => filter(f.getName))
-      .toList
-      .map(m => decode(m.toString.replaceFirst("^.*\\.", "")))  
-      .sort(_ < _)
-  
-  private def wrapped: AnyRef = x match {  
-    case x: Byte => byte2Byte(x)  
-    case x: Short => short2Short(x)  
-    case x: Char => char2Character(x)  
-    case x: Int => int2Integer(x)  
-    case x: Long => long2Long(x)  
-    case x: Float => float2Float(x)  
-    case x: Double => double2Double(x)  
-    case x: Boolean => boolean2Boolean(x)  
-    case _ => x.asInstanceOf[AnyRef]  
-  }  
-}  
-
-implicit def any2anyExtras(x: Any) = new AnyExtras(x)  
-
 import net.databinder.dispatch._
-import JsDef._
-import net.databinder.dispatch.couch_
+import net.databinder.dispatch.couch._
+import Js._
 
+val js = Js(""" { "a": {"b": 2} } """)
+
+trait Test extends Js {
+  val a = new Obj('a) {
+    val b = Num('b)
+  }
+}
