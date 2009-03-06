24ba336095c650a794f723f4174ef1e5d4e3f34d
Nathan Hamblen <nathan@technically.us>
2009-03-05 22:26:18 -0500

    testing 123

--- databinder-dispatch/project/build/src/Dispatch.scala
+++ databinder-dispatch/project/build/src/Dispatch.scala
@@ -2,7 +2,10 @@ import sbt._
 
 class Dispatch(info: ProjectInfo) extends DefaultProject(info)
 {
+  val scala_tools = "Scala Tools Releases" at "http://scala-tools.org/repo-releases"
+
   val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2"
+  val scala_test = "org.scala-tools.testing" % "scalatest" % "0.9.5"
   
   override def ivyXML =
     <publications>
---
+++ databinder-dispatch/src/test/scala/DispatchSpec.scala
@@ -0,0 +1,84 @@
+import org.scalatest.Spec
+import scala.collection.mutable.Stack
+
+class JsonSpec extends Spec {
+  import net.databinder.dispatch._
+  import Js._
+
+  val js = Js(""" { "a": {"a": "a string", "b": {"pi": 3.14159265 } }, "b": [1,2,3] } """)
+
+  describe("Parsed Json") {
+    it("should equal expected map") {
+      assert( js === Map(
+        'a -> Some(Map(
+          'a -> Some("a string"), 
+          'b -> Some(Map(
+            'pi -> Some(3.14159265)
+          ))
+        )), 
+        'b -> Some(List(Some(1), Some(2), Some(3)))
+      ))
+    }
+  }
+  describe("Layered extractor object") {
+    object TextExtractor extends Js {
+      val a = new Obj('a) {
+        val a = 'a ? str
+        val b = new Obj('b) {
+          val pi = 'pi ? num
+        }
+      }
+      val b = 'b ? list(num)
+    }
+    it("should match against top level object") {
+      val TextExtractor.a(a) = js
+      assert( a === js('a).get )
+    }
+    it("should match against second level string") {
+      val TextExtractor.a.a(a) = js
+      assert( a === "a string" )
+    }
+    it("should match against third level number") {
+      val TextExtractor.a.b.pi(p) = js
+      assert( p === 3.14159265 )
+    }
+    it("should match against a numeric list") {
+      val TextExtractor.b(b) = js
+      assert( b === List(1,2,3) )
+    }
+  }
+  describe("Flat extractors") {
+    val a = 'a ? obj
+    val b = 'b ? obj
+    val pi = 'pi ? num
+    val l = 'b ? list
+    it("should extract a top level object") {
+      val a(a_) = js
+      assert( a_ === js('a).get )
+    }
+    it("should deeply extract a third level number") {
+      val a(b(pi(pi_))) = js
+      assert( pi_ === 3.14159265 )
+    }
+    it("should match against an untyped list") {
+      val l(l_) = js
+      assert( l_ === List(Some(1), Some(2), Some(3)))
+    }
+  }
+  describe("Function extractor") {
+    def fun[T](ext: Js#M => T) = ext(js)
+    it("should extract a top level object") {
+      assert( fun('a ! obj) === js('a).get )
+    }
+    it("should extract a second level string") {
+      assert( fun { ('a ! obj) andThen ('a ! str) } === "a string" )
+    }
+    it("should extract a third level number") {
+      assert( fun { ('a ! obj) andThen ('b ! obj) andThen ('pi ! num) } === 3.14159265 )
+    }
+    it("should work with map") {
+      assert( List(js, js, js).map ('b ! list(num)) === List.tabulate(3, _ => List(1,2,3)) )
+    }
+  }
+}
+
