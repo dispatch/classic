0620f7d62acadcddc586629b692b86dc2872aef1
Nathan Hamblen <nathan@technically.us>
2009-03-13 00:03:34 -0400

    tweet, tweet

---
+++ src/main/scala/dispatch/Twitter.scala
@@ -0,0 +1,34 @@
+package dispatch.twitter
+
+import json._
+
+trait Twitter extends Js {
+  lazy val http = new Http("twitter.com")
+
+  def apply(action: String, params: Map[String, Any]) = http(
+    ("" :: service :: action :: Nil).mkString("/")
+  ) ?< (Map("count" -> count) ++ params)
+
+  def apply(action: String): Http#Request = this(action, Map[String, Any]())
+
+  val count = 200
+  val service: String
+}
+
+
+object Status extends Js {
+  val user = new Obj('user) {
+    val followers_count = 'followers_count ? num
+    val screen_name = 'screen_name ? str
+  }
+  val text = 'text ? str
+}
+
+class Statuses extends Twitter {
+  val service = "statuses"
+  
+  def public_timeline = 
+    this("public_timeline.json") $ (list ! obj)
+  def user_timeline(user: String) =
+    this("user_timeline/" + user + ".json") $ (list ! obj)
+}
