7b01d36907d0b788f99b4ebcd788795a951599c3
Nathan Hamblen <nathan@technically.us>
2009-03-29 00:51:23 -0400

    Http stream test

--- src/test/scala/HttpSpec.scala
+++ src/test/scala/HttpSpec.scala
@@ -7,13 +7,18 @@ class HttpSpec extends Spec with ShouldMatchers {
   val jane = "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.\n"
   val tus = new Http("technically.us")
   
-  describe("Expected text") {
-    it("should equal singleton fetched string") {
-      Http("http://technically.us/test.text").as_str should equal (jane)
+  describe("Singleton Http test get") {
+    endpoint_tests(Http("http://technically.us/test.text"))
+  }
+  describe("Bound host get") {
+    endpoint_tests(tus("/test.text"))
+  }
+  def endpoint_tests(test: Http#Request) = {
+    it("should equal expected string") {
+      test.as_str should equal (jane)
     }
-    it("should equal bound-host fetched string") {
-      tus("/test.text").as_str should equal (jane)
+    it("should stream to expected sting") {
+      test >> { str => io.Source.fromInputStream(str).mkString } should equal (jane)
     }
   }
-  
 }
