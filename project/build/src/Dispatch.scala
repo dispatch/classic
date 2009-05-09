e201c99d0b111f6aa81e4089bb72062c132a2406
Nathan Hamblen <nathan@technically.us>
2009-05-09 09:50:52 -0400

    publications no longer needed

--- project/build/src/Dispatch.scala
+++ project/build/src/Dispatch.scala
@@ -7,9 +7,4 @@ class Dispatch(info: ProjectInfo) extends DefaultProject(info)
   val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2"
   val scala_test = "org.scala-tools.testing" % "scalatest" % "0.9.5"
   val configgy = "net.lag" % "configgy" % "1.2.1a"
-  
-  override def ivyXML =
-    <publications>
-      <artifact name="dispatch" type="jar" ext="jar"/>
-    </publications>
 }
