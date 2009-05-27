c21e647b66ddf9eb8c08262736c5d4bd3faa0806
Nathan Hamblen <nathan@technically.us>
2009-05-27 09:00:56 -0400

    publishing info for databinder.net

--- project/build/src/Dispatch.scala
+++ project/build/src/Dispatch.scala
@@ -7,4 +7,7 @@ class Dispatch(info: ProjectInfo) extends DefaultProject(info)
   val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2"
   val scala_test = "org.scala-tools.testing" % "scalatest" % "0.9.5"
   val configgy = "net.lag" % "configgy" % "1.2.1a"
+
+  override def managedStyle = ManagedStyle.Maven
+  lazy val publishTo = Resolver.file("Databinder Repository", new java.io.File("/var/dbwww/repo"))
 }
