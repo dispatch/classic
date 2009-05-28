1f104008f9fe0ff55d654be762ee0e7035b1d0ec
Nathan Hamblen <nathan@technically.us>
2009-05-27 23:22:42 -0400

    use test configuration for scala-test

--- project/build/src/Dispatch.scala
+++ project/build/src/Dispatch.scala
@@ -4,9 +4,13 @@ class Dispatch(info: ProjectInfo) extends DefaultProject(info)
 {
   val lag_net = "lag.net repository" at "http://www.lag.net/repo"
 
+  val testConfig = Configurations.Test
+  val defaultConfig = Configurations.Default
+
   val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2"
-  val scala_test = "org.scala-tools.testing" % "scalatest" % "0.9.5"
   val configgy = "net.lag" % "configgy" % "1.3"
+
+  val st = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
 
   override def managedStyle = ManagedStyle.Maven
   lazy val publishTo = Resolver.file("Databinder Repository", new java.io.File("/var/dbwww/repo"))
