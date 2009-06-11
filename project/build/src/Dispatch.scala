527941e36008a2cd8f163066a379b6b8712d50a1
Nathan Hamblen <nathan@technically.us>
2009-06-11 00:12:31 -0400

    cross compile

--- project/build/src/Dispatch.scala
+++ project/build/src/Dispatch.scala
@@ -5,6 +5,7 @@ class Dispatch(info: ProjectInfo) extends DefaultProject(info)
   val lag_net = "lag.net repository" at "http://www.lag.net/repo"
 
   override def useMavenConfigurations = true
+  override def crossScalaVersions = Set("2.7.3", "2.7.4", "2.7.5")
   
   val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2" % "compile->default"
   val configgy = "net.lag" % "configgy" % "1.3" % "provided->default"
