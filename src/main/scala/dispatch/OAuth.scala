f9bff432ac20c0dfb0efe462f11db4dda19ad142
Nathan Hamblen <nathan@technically.us>
2009-06-13 22:16:55 -0400

    initial (working\!) oauth checkin

---
+++ src/main/scala/dispatch/OAuth.scala
@@ -0,0 +1,39 @@
+package dispatch.oauth
+import Http.{%, q_str}
+
+import collection.immutable.TreeMap
+
+import javax.crypto
+
+import org.apache.http.protocol.HTTP.UTF_8
+import org.apache.http.client.utils.URLEncodedUtils.format
+import org.apache.commons.codec.binary.Base64.encodeBase64
+
+case class Consumer(key: String, secret: String)
+
+object OAuth {
+  def sign(url: String, consumer: Consumer) = {
+    val params = TreeMap(
+      "oauth_consumer_key" -> consumer.key,
+      "oauth_signature_method" -> "HMAC-SHA1",
+      "oauth_version" -> "1.0",
+      "oauth_timestamp" -> (System.currentTimeMillis / 1000).toString,
+      "oauth_nonce" -> System.nanoTime.toString
+    )
+    val message = %&("GET" :: url :: q_str(params) :: Nil)
+    println(message)
+    
+    val SHA1 = "HmacSHA1";
+    val key_str = %&(consumer.secret :: "" :: Nil)
+    val key = new crypto.spec.SecretKeySpec(key_str.getBytes(UTF_8), SHA1)
+    val sig = {
+      val mac = crypto.Mac.getInstance(SHA1)
+      mac.init(key)
+      new String(encodeBase64(mac.doFinal(bytes(message))))
+    }
+    params + ("oauth_signature" -> sig)
+  }
+
+  def %& (s: Seq[String]) = s map % mkString "&"
+  def bytes(str: String) = str.getBytes(UTF_8)
+}
