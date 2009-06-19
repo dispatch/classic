67989f8fc2f98fc7114d4c49ddf541609b9a598a
Nathan Hamblen <nathan@technically.us>
2009-06-18 23:25:31 -0400

    pulling sign params from HttpRequestBase

--- src/main/scala/dispatch/OAuth.scala
+++ src/main/scala/dispatch/OAuth.scala
@@ -2,28 +2,21 @@ package dispatch.oauth
 import Http.{%, q_str}
 
 import collection.Map
-import collection.immutable.TreeMap
+import collection.immutable.{TreeMap, Map=>IMap}
 
 import javax.crypto
 
 import org.apache.http.protocol.HTTP.UTF_8
 import org.apache.commons.codec.binary.Base64.encodeBase64
+import org.apache.http.client.methods.HttpRequestBase
 
 case class Consumer(key: String, secret: String)
 case class Token(value: String, secret: String)
 
-object OA { // scalac thinks this method is recurisve if it's in OAuth (?)
-  implicit def add_<<@ (r: Request) = new {
-    def <<@ (consumer: Consumer, token: Option[Token]) = r next {
-      case before: Post =>
-        val after = new Post(OAuth.sign(before.getMethod, before.getURI.toString, before.values, consumer, token))
-        r.mimic(after)(before)
-      case before => before
-    }
-  }
-}
-
+/** Import this object's methods to add signing operators to dispatch.Request */
 object OAuth {
+  implicit def Requst2RequestSigner(r: Request) = new RequestSigner(r)
+  
   def sign(method: String, url: String, user_params: Map[String, Any], consumer: Consumer, token: Option[Token]) = {
     val params = TreeMap(
       "oauth_consumer_key" -> consumer.key,
@@ -34,7 +27,6 @@ object OAuth {
     ) ++ token.map { "oauth_token" -> _.value } ++ user_params
     
     val message = %%(method :: url :: q_str(params) :: Nil)
-    println(message)
     
     val SHA1 = "HmacSHA1";
     val key_str = %%(consumer.secret :: (token map { _.secret } getOrElse "") :: Nil)
@@ -49,4 +41,29 @@ object OAuth {
 
   def %% (s: Seq[String]) = s map % mkString "&"
   def bytes(str: String) = str.getBytes(UTF_8)
+  
+  class RequestSigner(r: Request) {
+    /** Convert to a post before signing */
+    val <<@ = r << IMap() <@ (_, _)
+    
+    /** Sign request using Post (<<) parameters and query string */
+    def <@ (consumer: Consumer, token: Option[Token]) = r next {
+      case before: Post =>
+        r.mimic(new Post(OAuth.sign(
+          before.getMethod, oauth_url(r, before), params(before) ++ before.values, consumer, token))
+        )(before)
+      case before =>
+        val signed = OAuth.sign(before.getMethod, oauth_url(r, before), params(before), consumer, token)
+        before.setURI(java.net.URI.create(oauth_url(r, before) + (Http ? signed)))
+        before
+    }
+  }
+  private def oauth_url(r: Request, req: HttpRequestBase) = r.host.getOrElse("") + req.getURI.toString.split('?')(0)
+
+  private def params(req: HttpRequestBase): IMap[String,Any] = req.getURI.getQuery match {
+      case null => IMap.empty
+      case query => IMap.empty ++ query.split('&').map {
+        _ split "=" match { case Seq(name, value) => name -> value }
+      }
+  }
 }
