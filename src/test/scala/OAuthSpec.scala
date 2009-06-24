22ec9d4b4efba632d8d62c61799d5f9fa7884961
Nathan Hamblen <nathan@technically.us>
2009-06-23 22:13:40 -0400

    oauth specs through test service

---
+++ src/test/scala/OAuthSpec.scala
@@ -0,0 +1,22 @@
+import org.scalatest.{Spec, BeforeAndAfter}
+import org.scalatest.matchers.ShouldMatchers
+
+class OAuthSpec extends Spec with ShouldMatchers with BeforeAndAfter {
+  import dispatch._
+  import oauth._
+  import OAuth._
+  
+  val svc = :/("term.ie") / "oauth" / "example"
+  val consumer = Consumer("key", "secret")
+  
+  describe("OAuth test host") {
+    it("should echo parameters from protected service") {
+      val h = new Http
+      val request_token = h(svc / "request_token.php" <@ consumer as_token)
+      val access_token = h(svc / "access_token" <@ (consumer, request_token) as_token)
+      h(
+        svc / "echo_api.php" <<? Map("top"->"secret") <@ (consumer, access_token) as_str
+      ) should be ("top=secret")
+    }
+  }
+}
