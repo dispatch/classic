0cf82ad16c91e3e3f1d925c84a984655d7accbba
Nathan Hamblen <nathan@technically.us>
2009-07-06 08:10:08 -0400

    fix sxr publishing output/input path

--- project/build/DispatchProject.scala
+++ project/build/DispatchProject.scala
@@ -18,7 +18,7 @@ class DispatchProject(info: ProjectInfo) extends DefaultProject(info) with AutoC
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
   
-  def sxrPackagePath = outputPath / (artifactBaseName + ".sxr")
+  def sxrPackagePath = outputPath / "classes.sxr"
   def sxrPublishPath = Path.fromFile("/var/dbwww/sxr") / name / projectVersion.value.toString
   lazy val publishSxr = syncTask(sxrPackagePath, sxrPublishPath) dependsOn(compile)
   override def publishAction = super.publishAction dependsOn(publishSxr)
