e94ece22209940b9659059b90c9fdc7375fef954
Nathan Hamblen <nathan@technically.us>
2009-07-12 19:10:20 -0400

    move sorces and tests into  sub-projects

--- project/build/DispatchProject.scala
+++ project/build/DispatchProject.scala
@@ -1,29 +1,36 @@
 import sbt._
 
-class DispatchProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins
+class DispatchProject(info: ProjectInfo) extends ParentProject(info)
 {
-  val lag_net = "lag.net repository" at "http://www.lag.net/repo"
-
   override def useDefaultConfigurations = true
   override def crossScalaVersions = Set("2.7.3", "2.7.4", "2.7.5")
-  
-  val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2"
-  val configgy = "net.lag" % "configgy" % "1.3" % "provided->default"
+    
+  class HttpProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
+    val httpclient = "org.apache.httpcomponents" % "httpclient" % "4.0-beta2"
+    val lag_net = "lag.net repository" at "http://www.lag.net/repo"
+    val configgy = "net.lag" % "configgy" % "1.3" % "provided->default"
+    val st = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
+ 
+    val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.1")
+  }
 
-  val st = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
-
-  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.1")
+  lazy val http = project("http", "Dispatch HTTP Core", new HttpProject(_))
+  lazy val json = project("json", "Dispatch JSON Extraction", http)
+  lazy val oauth = project("oauth", "Dispatch OAuth Signing", http)
+  lazy val times = project("times", "Dispatch NY Times Interface", json)
+  lazy val couch = project("couch", "Dispatch CouchDB Interface", json)
+  lazy val twitter = project("twitter", "Dispatch Twitter Interface", json, oauth)
   
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
   
-  def sxrMainPath = outputPath / "classes.sxr"
+/*  def sxrMainPath = outputPath / "classes.sxr"
   def sxrTestPath = outputPath / "test-classes.sxr"
   def sxrPublishPath = Path.fromFile("/var/dbwww/sxr") / name / projectVersion.value.toString
   lazy val publishSxr = 
     syncTask(sxrMainPath, sxrPublishPath / "main") dependsOn(
       syncTask(sxrTestPath, sxrPublishPath / "test") dependsOn(testCompile)
     )
-  override def publishAction = super.publishAction dependsOn(publishSxr)
+  override def publishAction = super.publishAction dependsOn(publishSxr) */
 }
