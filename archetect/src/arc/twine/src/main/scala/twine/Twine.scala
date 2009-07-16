5c58bdecc2ef869f757f9ae189d9671badf54398
Nathan Hamblen <nathan@technically.us>
2009-07-16 02:10:33 -0400

    dispatch references

--- archetect/src/arc/twine/src/main/scala/twine/Twine.scala
+++ archetect/src/arc/twine/src/main/scala/twine/Twine.scala
@@ -1,9 +1,11 @@
 package twine
 
 import dispatch._
+import twitter._
 
 object Twine {
   def main(args: Array[String]) {
-    println("a ball of wax")
+    val http = new Http
+    http(Search("#dbDispatch") >>> System.out)
   }
 }
