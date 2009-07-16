3ec0db6d3ece2a7e34221006b1368b148277f738
Nathan Hamblen <nathan@technically.us>
2009-07-16 08:44:25 -0400

    temporary and permanent dependencies

--- archetect/src/arc/twine/project/build/TwineProject.scala
+++ archetect/src/arc/twine/project/build/TwineProject.scala
@@ -2,5 +2,11 @@ import sbt._
 
 class TwineProject(info: ProjectInfo) extends DefaultProject(info) with extract.BasicSelfExtractingProject
 {
+  val lag_net = "lag.net repository" at "http://www.lag.net/repo"
+  val configgy = "net.lag" % "configgy" % "1.3" % "provided->default"
   val dispatch = "net.databinder" %% "dispatch-twitter" % "{{dispatch.version}}"
+  // rm below when published ivy.xml is correct
+  val dispatch_oauth = "net.databinder" %% "dispatch-oauth" % "{{dispatch.version}}"
+  val dispatch_json = "net.databinder" %% "dispatch-json" % "{{dispatch.version}}"
+  val dispatch_http = "net.databinder" %% "dispatch-http" % "{{dispatch.version}}"
 }
