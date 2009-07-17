8d5c649f810c0a7b21b792db9a119b91b8381632
Nathan Hamblen <nathan@technically.us>
2009-07-16 23:58:10 -0400

    implicit operators from string

--- literaljson/src/main/scala/dispatch/LiteralJson.scala
+++ literaljson/src/main/scala/dispatch/LiteralJson.scala
@@ -6,6 +6,8 @@ import literaljson.JsonParser._
 object Js {
   /** Add JSON-processing method ># to dispatch.Request */
   implicit def Request2JsonRequest(r: dispatch.Request) = new JsonRequest(r)
+  /** Add String conversion since Http#str2req implicit will not chain. */
+  implicit def String2JsonRequest(r: String) = new JsonRequest(new Request(r))
 
   class JsonRequest(r: Request) {
     /** Process response as JsValue in block */
