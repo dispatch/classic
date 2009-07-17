f712fce9ed7cff7d7b50bc23c30af7013a690593
Nathan Hamblen <nathan@technically.us>
2009-07-17 09:15:08 -0400

    configgy file

--- archetect/src/arc/twine/src/main/scala/twine/Twine.scala
+++ archetect/src/arc/twine/src/main/scala/twine/Twine.scala
@@ -2,9 +2,13 @@ package twine
 
 import dispatch._
 import twitter._
+import net.lag.configgy._
 
 object Twine {
   def main(args: Array[String]) {
+    val conf = new java.io.File(System.getProperty("user.home"), ".twine.conf")
+    conf.createNewFile()
+    Configgy.configure(conf.getPath)
     val http = new Http
     http(Search("#dbDispatch") >>> System.out)
   }
