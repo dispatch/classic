5b0bd68e772fb3d19807271fb7b4f019a25f419f
Nathan Hamblen <nathan@technically.us>
2009-07-19 14:08:29 -0400

    script making task

--- archetect/src/arc/twine/project/build/TwineProject.scala
+++ archetect/src/arc/twine/project/build/TwineProject.scala
@@ -9,4 +9,18 @@ class TwineProject(info: ProjectInfo) extends DefaultProject(info) with extract.
   val dispatch_oauth = "net.databinder" %% "dispatch-oauth" % "{{dispatch.version}}"
   val dispatch_json = "net.databinder" %% "dispatch-json" % "{{dispatch.version}}"
   val dispatch_http = "net.databinder" %% "dispatch-http" % "{{dispatch.version}}"
+  
+  // will use proguard to make one runnable jar later, for now a crazy long classpath will do
+  lazy val script = task {
+    import java.io.File
+    val twine = (info.projectPath / "twine").asFile
+    FileUtilities.write(twine,
+      "java -cp %s %s \"$@\"" format (
+        (Path.makeString(runClasspath.get) :: mainDependencies.scalaJars.get.toList).mkString(File.pathSeparator),
+        getMainClass(false).get
+      ), log) orElse {
+      twine setExecutable true
+      None
+    }
+  } dependsOn compile
 }
