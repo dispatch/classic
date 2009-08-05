f148a65581d47edcd4f7a6e2a03a405d5869ea5b
Nathan Hamblen <nathan@technically.us>
2009-08-05 17:10:43 -0400

    jorge's patch to fix the twitter spec

--- twitter/src/test/scala/TwitterSpec.scala
+++ twitter/src/test/scala/TwitterSpec.scala
@@ -3,13 +3,14 @@ import org.scalatest.matchers.ShouldMatchers
 
 class TwitterSpec extends Spec with ShouldMatchers {
   import dispatch._
+  import Http._
   import json.Js._
   import twitter._
   
   describe("Twitter Search") {
     val http = new Http
     it("should find tweets containing #dbDispatch") {
-      val res = http(Search("#dbDispatch").results)
+      val res = http(Search("#dbDispatch"))
       res.isEmpty should be (false)
       res map Status.text forall { _ contains "#dbDispatch" } should be (true)
     }
