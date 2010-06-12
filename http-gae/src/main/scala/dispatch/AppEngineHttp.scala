fc664bf0ad4cfd0fce78a89a696b8ba4c6057a9d
Nathan Hamblen <nathan@technically.us>
2010-06-12 14:31:01 -0400

    bind gae conn manager to gae client

--- http-gae/src/main/scala/dispatch/AppEngineHttp.scala
+++ http-gae/src/main/scala/dispatch/AppEngineHttp.scala
@@ -4,7 +4,7 @@ import dispatch.gae.GAEConnectionManager
 import org.apache.http.params.HttpParams
 import org.apache.http.conn.ClientConnectionManager
 
-class AppEngineConfiguredClient(conman: ClientConnectionManager) extends ConfiguredHttpClient(conman) {
+class AppEngineConfiguredClient extends ConfiguredHttpClient(GAEConnectionManager) {
   // NOOP
   override protected def configureProxy(params: HttpParams) = params
 }
@@ -12,5 +12,5 @@ class AppEngineConfiguredClient(conman: ClientConnectionManager) extends Configu
 object AppEngineHttp extends AppEngineHttp with HttpImplicits
 
 class AppEngineHttp extends Http {
-  override val client = new AppEngineConfiguredClient(GAEConnectionManager)
+  override val client = new AppEngineConfiguredClient
 }
