66a5358c665b7778a28638ce6b8e020a05b30674
Chris Lewis <chris@rosaloves.com>
2010-08-16 20:39:22 -0400

    additional AuthRequest factories, authorizer produces Handler, project name uniformity

--- google/src/main/scala/ClientLogin.scala
+++ google/src/main/scala/ClientLogin.scala
@@ -3,13 +3,18 @@ package dispatch.google.cl
 import dispatch._
 import dispatch.Http._
 
-/* http://code.google.com/apis/accounts/docs/AuthForInstalledApps.html */
+/**
+ * Google ClientLogin is a proprietary authorization mechanism for accessing
+ * protected user data: http://code.google.com/apis/accounts/docs/AuthForInstalledApps.html
+ */
 
+/* Google account types. */
 sealed abstract class AccountType(val name: String)
 case object Google extends AccountType("GOOGLE")
 case object Hosted extends AccountType("HOSTED")
 case object Ambiguous extends AccountType("HOSTED_OR_GOOGLE")
 
+/* A request for authorized access to a service on behalf of a user. */
 case class AuthRequest(service: String, email: String, password: String, a_type: AccountType, source: String) {
   def as_map = Map(
     "accountType" -> a_type.name,
@@ -22,6 +27,8 @@ case class AuthRequest(service: String, email: String, password: String, a_type:
 object AuthRequest {
   def apply(service: String, email: String, password: String): AuthRequest =
     AuthRequest(service, email, password, Ambiguous, "dispatch")
+  def apply(service: String, email: String, password: String, a_type: AccountType): AuthRequest =
+    AuthRequest(service, email, password, a_type, "dispatch")
 }
 
 case class Token(value: String)
@@ -30,18 +37,18 @@ object ClientLogin {
   val host = :/("www.google.com")
   val svc = host.secure / "accounts" / "ClientLogin"
   
-  /* Extract an authorized token from a response source. */
+  /* Monadic token extraction. */
   def auth_token(src: io.Source): Option[Token] =
     src.getLines.find { _ startsWith "Auth" } map { l => Token(l drop 5) }
   
   class RequestSigner(r: Request) {
     def <@(token: Token) = r <:< Map("Authorization" -> ("GoogleLogin auth=" + token.value))
+    /* Extract an authorized token from a response source. */
+    def authorizer = r >~ { auth_token(_).getOrElse { error("Authorization response contained no token!") }}
   }
   
   implicit def AuthRequest2Request(a_req: AuthRequest) = svc << a_req.as_map
-  
+  implicit def AuthRequest2RequestSigner(a_req: AuthRequest) = new RequestSigner(svc << a_req.as_map)
   implicit def Request2RequestSigner(r: Request) = new RequestSigner(r)
-  
-  //TODO captcha ..
   
 }
--- google/src/test/scala/ClientLoginSpec.scala
+++ google/src/test/scala/ClientLoginSpec.scala
@@ -16,10 +16,9 @@ object ClientLoginSpec extends Specification {
     "find contacts" in {
       val http = new Http
       val auth_req = AuthRequest("cp", email, password)
-
-      val res = http(auth_req >~ auth_token) map { t: Token =>
-        http(:/("www.google.com") / "m8" / "feeds" / "contacts" / auth_req.email / "full" <@ t <> { _ \\ "feed" })
-      }
+      
+      val t = http(auth_req authorizer)
+      val res = http(:/("www.google.com") / "m8" / "feeds" / "contacts" / auth_req.email / "full" <@ t <> { _ \\ "feed" })
       
       res must notBeEmpty
       res map { _ \ "entry" must notBeEmpty }
