31fb44199750ca25c9a75813a4d545aa6d3728cf
Nathan Hamblen <nathan@technically.us>
2010-12-06 00:30:24 -0500

    Source for userstream

--- twitter/src/main/scala/dispatch/Streaming.scala
+++ twitter/src/main/scala/dispatch/Streaming.scala
@@ -3,23 +3,23 @@ package dispatch.twitter
 import dispatch._
 import dispatch.oauth._
 import OAuth._
-import net.liftweb.json.{JsonAST,JsonParser}
-import JsonAST._
+import net.liftweb.json.JsonParser.parse
+import net.liftweb.json.JsonAST.JValue
+import scala.io.Source
 import java.io.{InputStreamReader,BufferedReader}
 
 object UserStream {
   val host = :/("userstream.twitter.com").secure
   val svc = host / "2" / "user.json"
-  type Listener = JValue => Unit
   def open(cons: Consumer, tok: Token, since_id: Option[String])
-          (make_listener: JValue => Listener) = {
+          (make_listener: JValue => JValue => Unit) = {
     val req = svc <<? (Map.empty ++ since_id.map { id => "since_id" -> id })
     req <@ (cons, tok) >> { (stm, charset) =>
-      val reader = new BufferedReader(new InputStreamReader(stm, charset))
-      val listener = make_listener(JsonParser.parse(reader.readLine()))
-      while (true) {
-        listener(JsonParser.parse(reader.readLine()))
-      }
+      val lines = Source.fromInputStream(stm, charset).getLines.filter {
+        ! _.isEmpty
+      }.map(parse)
+      val listener = make_listener(lines.next())
+      lines.foreach(listener)
     }
   }
 }
