ba753867d37277950d723b6f07e68879a81e3f97
Nathan Hamblen <nathan@technically.us>
2010-12-21 23:39:22 -0500

    extract reactor, handler, etc

--- nio/src/main/scala/nio.scala
+++ nio/src/main/scala/nio.scala
@@ -11,49 +11,65 @@ import org.apache.http.params.CoreProtocolPNames;
 import java.net.InetSocketAddress
 
 class HttpNio extends dispatch.HttpExecutor {
+  val http_params = make_params
+  def make_params =
+    (new org.apache.http.params.SyncBasicHttpParams)
+      .setIntParameter(CoreConnectionPNames.SO_TIMEOUT, 5000)
+      .setIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 10000)
+      .setIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, 8 * 1024)
+      .setBooleanParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false)
+      .setBooleanParameter(CoreConnectionPNames.TCP_NODELAY, true)
+
+  def execution_handler =
+    new org.apache.http.nio.protocol.NHttpRequestExecutionHandler {
+      val request_object = "request_object"
+      def submitRequest(ctx: HttpContext) = {
+        val flag = "umm_request_already_submitted"
+        if (ctx.getAttribute(flag) == null) {
+          ctx.setAttribute(flag, true)
+          println("submitting request")
+          ctx.getAttribute(request_object).asInstanceOf[HttpRequest]
+        } else null
+      }
+      def finalizeContext(ctx: HttpContext) { }
+      def handleResponse(response: HttpResponse, ctx: HttpContext) { }
+      def initalizeContext(ctx: HttpContext, attachment: Any) {
+        ctx.setAttribute(request_object, attachment)
+      }
+      def responseEntity(res: HttpResponse, ctx: HttpContext) =
+        new org.apache.http.nio.entity.BufferingNHttpEntity(
+          res.getEntity, new org.apache.http.nio.util.DirectByteBufferAllocator)
+    }
+
+  def http_handler =
+    new org.apache.http.nio.protocol.AsyncNHttpClientHandler(
+      new ImmutableHttpProcessor(Array(
+        new RequestContent(),
+        new RequestTargetHost(),
+        new RequestConnControl(),
+        new RequestUserAgent(),
+        new RequestExpectContinue()
+      )),
+      execution_handler,
+      new org.apache.http.impl.DefaultConnectionReuseStrategy,
+      http_params
+    )
+
+  def worker_count = 4
+  val io_reactor = new DefaultConnectingIOReactor(worker_count, http_params)
+  val reactor_thread = start_reactor_thread
+  def start_reactor_thread =
+    (new Thread(new Runnable {
+      def run { 
+        io_reactor.execute(new DefaultClientIOEventDispatch(http_handler, http_params))
+      }
+    })).start()
   
   val execute: (HttpHost, Option[Credentials], HttpRequest) => HttpResponse = {
     case (host, Some(creds), req) => error("todo")
     case (host, _, req) =>
-      val params = new org.apache.http.params.SyncBasicHttpParams
-      params
-          .setIntParameter(CoreConnectionPNames.SO_TIMEOUT, 5000)
-          .setIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 10000)
-          .setIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, 8 * 1024)
-          .setBooleanParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false)
-          .setBooleanParameter(CoreConnectionPNames.TCP_NODELAY, true)
 
-      val handler = new org.apache.http.nio.protocol.AsyncNHttpClientHandler(
-        new ImmutableHttpProcessor(Array(
-          new RequestContent(),
-          new RequestTargetHost(),
-          new RequestConnControl(),
-          new RequestUserAgent(),
-          new RequestExpectContinue())),
-        new org.apache.http.nio.protocol.NHttpRequestExecutionHandler {
-          def submitRequest(ctx: HttpContext) = {
-            val flag = "umm_request_already_submitted"
-            if (ctx.getAttribute(flag) == null) {
-              ctx.setAttribute(flag, true)
-              println("submitting request")
-              req
-            } else null
-          }
-          def finalizeContext(ctx: HttpContext) { }
-          def handleResponse(response: HttpResponse, ctx: HttpContext) { }
-          def initalizeContext(ctx: HttpContext, attachment: Any) { }
-          def responseEntity(res: HttpResponse, ctx: HttpContext) =
-            new org.apache.http.nio.entity.BufferingNHttpEntity(
-              res.getEntity, new org.apache.http.nio.util.DirectByteBufferAllocator)
-        },
-        new org.apache.http.impl.DefaultConnectionReuseStrategy,
-        params
-      )
-      val ioReactor = new DefaultConnectingIOReactor(2, params)
-      (new Thread(new Runnable {
-        def run { ioReactor.execute(new DefaultClientIOEventDispatch(handler, params)) }
-      })).start()
-      ioReactor.connect(
+      io_reactor.connect(
         new InetSocketAddress(
           host.getHostName, 
           if (host.getPort == -1) 80 else host.getPort),
@@ -62,7 +78,7 @@ class HttpNio extends dispatch.HttpExecutor {
         null
       )
       Thread.sleep(3000)
-      ioReactor.shutdown()
+      io_reactor.shutdown()
       error("help")
   }
   /** Unadorned handler return type */
