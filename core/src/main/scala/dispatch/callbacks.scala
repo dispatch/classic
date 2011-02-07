2defb1e0cb95e016cf0f68e232541aabfb17570d
Nathan Hamblen <nathan@technically.us>
2011-02-06 20:41:39 -0500

    do everything in the consumer, rf finished callback

--- core/src/main/scala/dispatch/callbacks.scala
+++ core/src/main/scala/dispatch/callbacks.scala
@@ -6,7 +6,10 @@ object Callback {
   type Function = (HttpResponse, Array[Byte], Int) => Unit
   type Finish[T] = HttpResponse => T
 
-  def strings[T](req: Request, block: (String => Unit), finish: Finish[T]) = Callback(
+  def apply(request: Request, function: Callback.Function): Callback[Unit] = 
+    Callback(request, function, { _ => () })
+
+  def strings[T](req: Request, block: (String => Unit)) = Callback(
     req,
     (res, bytes, len) => {
       val charset = for {
@@ -15,15 +18,14 @@ object Callback {
         param <- Option(elem.getParameterByName("charset"))
       } yield param.getValue()
       block(new String(bytes, 0, len, charset.getOrElse(Request.factoryCharset)))
-    },
-    finish
+    }
   )
 
   /** Divide input up by given regex. Buffers across inputs so strings are
    * only split on the divider, and handles any leftovers in finish. Skips
    * empty strings. */
   def stringsBy[T](divider: String)
-                  (req: Request, block: (String => Unit), finish: Finish[T]) = {
+                  (req: Request, block: (String => Unit)) = {
     var buffer = ""
     strings(
       req,
@@ -31,20 +33,23 @@ object Callback {
         val strings = (buffer + string).split(divider, -1)
         strings.take(strings.length - 1).filter { !_.isEmpty }.foreach(block)
         buffer = strings.last
-      }, 
-      { res =>
-        if (!buffer.isEmpty) block(buffer)
-        finish(res)
       }
-    )
+    ) ^> { res => 
+      if (!buffer.isEmpty) block(buffer)
+    }
   }
   /** callback transformer for strings split on the newline character, newline removed */
   def lines[T] = stringsBy[T]("[\n\r]+")_
 }
 
 case class Callback[T](request: Request, 
-                       function: Callback.Function, 
-                       finish: Callback.Finish[T])
+                  function: Callback.Function, 
+                  finish: Callback.Finish[T]) {
+  def ^> [T](finish: Callback.Finish[T]) = Callback(request, function, { res =>
+    this.finish(res)
+    finish(res)
+  })
+}
 
 trait ImplicitCallbackVerbs {
   implicit def toCallbackVerbs(req: Request) = new CallbackVerbs(req)
@@ -54,12 +59,8 @@ object CallbackVerbs extends ImplicitCallbackVerbs
 
 class CallbackVerbs(subject: Request) {
   import Callback._
-  val nf: Callback.Finish[Unit] = _ => ()
-  def ^[T](callback: Function, finish: Finish[T] = nf) =
-    Callback(subject, callback, finish)
-  def ^-[T](callback: (String => Unit), finish: Finish[T] = nf) =
-    strings(subject, callback, finish)
+  def ^[T](callback: Function) = Callback(subject, callback)
+  def ^-[T](callback: String => Unit) = strings(subject, callback)
   /** strings split on the newline character, newline removed */
-  def ^--[T](callback: (String => Unit), finish: Finish[T] = nf) =
-    lines(subject, callback, finish)
+  def ^--[T](callback: String => Unit) = lines(subject, callback)
 }
--- nio/src/main/scala/nio.scala
+++ nio/src/main/scala/nio.scala
@@ -6,13 +6,14 @@ import org.apache.http.message.BasicHttpEntityEnclosingRequest
 import org.apache.http.protocol._
 import org.apache.http.impl.nio.client.{DefaultHttpAsyncClient,BasicHttpAsyncRequestProducer=>Producer}
 import org.apache.http.client.methods._
-import org.apache.http.params.CoreConnectionPNames;
-import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.CoreConnectionPNames
+import org.apache.http.params.CoreProtocolPNames
 import org.apache.http.nio.{ContentDecoder,IOControl,NHttpConnection}
-import org.apache.http.nio.entity.ConsumingNHttpEntity
-import org.apache.http.impl.nio.client.BasicHttpAsyncResponseConsumer
+import org.apache.http.nio.entity.{ConsumingNHttpEntity,BufferingNHttpEntity}
+import org.apache.http.nio.client.HttpAsyncResponseConsumer
 import org.apache.http.nio.concurrent.FutureCallback
-import java.net.InetSocketAddress
+import org.apache.http.nio.util.HeapByteBufferAllocator
+import java.util.concurrent.Future
 import java.io.IOException
 
 object Http {
@@ -25,30 +26,46 @@ class Http extends dispatch.HttpExecutor {
 
   type HttpPackage[T] = dispatch.futures.StoppableFuture[T]
 
-  class StoppableConsumer extends BasicHttpAsyncResponseConsumer {
-    @volatile private var stopped = false
+  trait StoppableConsumer[T] extends HttpAsyncResponseConsumer[T] {
+    @volatile var stopping = false
     final override def consumeContent(decoder: ContentDecoder, ioctrl: IOControl) {
-      if (stopped) {
+      if (stopping) {
         ioctrl.shutdown()
         cancel()
       }
       else consume(decoder, ioctrl)
     }
-    def consume(decoder: ContentDecoder, ioctrl: IOControl) {
-      super.consumeContent(decoder, ioctrl)
+    @volatile var response: Option[HttpResponse] = None
+    def responseReceived(res: HttpResponse) {
+      response = Some(res)
     }
-    def stop() { stopped = true }
-  }
+    def consume(decoder: ContentDecoder, ioctrl: IOControl)
+    def stop() { stopping = true }
+    @volatile var result: Option[T] = None
+    def responseCompleted() {
+      result = completeResult(response)
+    }
+    def completeResult(response: Option[HttpResponse]): Option[T]
+    def getResult: T = result.get
 
-  class ConsumerFuture(
-    underlying: Future, 
-    consumer: StoppableConsumer
+    def failed(ex: Exception) {
+      println(ex)
+    }
+  }
+  class EmptyCallback[T] extends FutureCallback[T] {
+    def cancelled() { }
+    def completed(res: T) { }
+    def failed(ex: Exception) { }
+  }
+  class ConsumerFuture[T](
+    underlying: Future[T], 
+    consumer: StoppableConsumer[T]
   ) extends dispatch.futures.StoppableFuture[T] {
-    def apply() = { fut.get(); result.get }
-    def isSet = fut.isDone
+    def apply() = underlying.get()
+    def isSet = underlying.isDone
     def stop() = { 
       consumer.stop()
-      fut.cancel(true)
+      underlying.cancel(true)
     }
   }
 
@@ -57,25 +74,32 @@ class Http extends dispatch.HttpExecutor {
     credsopt.map { creds =>
       error("todo")
     } getOrElse {
-      var result: Option[T] = None
-      val consumer = new StoppableConsumer
-      val context = new BasicHttpContext
-      val fut = client.execute(
-        new Producer(host, req), 
-        consumer,
-        new FutureCallback[HttpResponse] {
-          var isCancelled = false
-          def cancelled() {
-            isCancelled = true
+      val consumer = new StoppableConsumer[T] {
+        @volatile var entity: Option[ConsumingNHttpEntity] = None
+        def consume(decoder: ContentDecoder, ioctrl: IOControl) { synchronized {
+          entity = entity.orElse {
+            for {
+              res <- response
+              ent <- Option(res.getEntity)
+            } yield (new BufferingNHttpEntity(ent, new HeapByteBufferAllocator))
           }
-          def completed(res: HttpResponse) { 
-            result = Some(block(res))
+          entity.map { _.consumeContent(decoder, ioctrl) }
+        } }
+        def completeResult(response: Option[HttpResponse]) = {
+          for (res <- response; ent <- entity) {
+            res.setEntity(ent)
+            ent.finish()
           }
-          def failed(ex: Exception) { ex.printStackTrace() }
+          response.map(block)
         }
-      )
-      new WrappedFuture
-    }                     
+        def cancel() {
+          entity.map { _.finish() }
+        }
+      }
+      val context = new BasicHttpContext
+      val fut = client.execute(new Producer(host, req), consumer, new EmptyCallback[T])
+      new ConsumerFuture(fut, consumer)
+    }
   }
   
   def executeWithCallback[T](host: HttpHost, credsopt: Option[dispatch.Credentials], 
@@ -84,30 +108,19 @@ class Http extends dispatch.HttpExecutor {
       error("todo")
     } getOrElse {
       val ioc = DecodingCallback(callback)
-      var result: Option[T] = None
-      val consumer = new StoppableConsumer {
+      val consumer = new StoppableConsumer[T] {
         override def responseReceived(res: HttpResponse) {
           response = Some(res)
         }
         override def consume(decoder: ContentDecoder, ioctrl: IOControl) {
           ioc.with_decoder(response.get, decoder)
         }
+        def completeResult(response: Option[HttpResponse]) = 
+          response.map(callback.finish)
+        def cancel() { }
       }
-
-      val fut = client.execute(new Producer(host, req), 
-                               consumer,
-                               new FutureCallback[HttpResponse] {
-        def cancelled() { }
-        def completed(res: HttpResponse) {
-          result = Some(ioc.callback.finish(response))
-        }
-        def failed(ex: Exception) { ex.printStackTrace() }
-      })
-      new dispatch.futures.StoppableFuture[T] {
-        def apply() = { fut.get(); result.get }
-        def isSet = fut.isDone
-        def stop() = { consumer.stop(); fut.cancel(true) }
-      }
+      val fut = client.execute(new Producer(host, req), consumer, new EmptyCallback[T])
+      new ConsumerFuture(fut, consumer)
     }
   }
 
