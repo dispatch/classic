05baabfa578dede0d0b1fd92a3eb954fc5ecc2df
Nathan Hamblen <nathan@technically.us>
2011-04-07 16:09:32 -0400

    new posterous version

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,9 +4,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   val archetect = "net.databinder" % "archetect-plugin" % "0.1.3"
 
-  val snuggletex_repo = "snuggletex_repo" at "http://www2.ph.ed.ac.uk/maven2"
-  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-  val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.7"
 
   val sxr_publish = "net.databinder" % "sxr-publish" % "0.2.0"
 }
