d3c142042acd1bc12e47aeb583ef2e8afbce26dc
Nathan Hamblen <nathan@technically.us>
2011-05-06 09:15:56 -0400

    try dispatch

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -11,7 +11,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val lessis = "less is repo" at "http://repo.lessis.me"
   val ghIssues = "me.lessis" % "sbt-gh-issues" % "0.1.0"
 
-  val pf = "net.databinder" % "pamflet-plugin" % "0.1.0"
+  val pf = "net.databinder" % "pamflet-plugin" % "0.1.1-SNAPSHOT"
 
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
