f511ee3f4ef6e02d3dc62176f1d255516b72c1bd
Nathan Hamblen <nathan@technically.us>
2011-06-04 00:24:21 -0400

    pamflet 0.2.0

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -11,7 +11,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val lessis = "less is repo" at "http://repo.lessis.me"
   val ghIssues = "me.lessis" % "sbt-gh-issues" % "0.1.0"
 
-  val pf = "net.databinder" % "pamflet-plugin" % "0.1.4"
+  val pf = "net.databinder" % "pamflet-plugin" % "0.2.0"
 
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
