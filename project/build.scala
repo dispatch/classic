d66eafb925eb868a31e2e03644522d1d7214d6d7
Nathan Hamblen <nathan@technically.us>
2011-11-24 23:21:41 -0500

    whitespace

--- project/build.scala
+++ project/build.scala
@@ -5,24 +5,32 @@ object Dispatch extends Build {
   val shared = Defaults.defaultSettings ++ Seq(
     organization := "net.databinder",
     version := "0.8.6",
-    crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1"),
+    crossScalaVersions :=
+      Seq("2.8.0", "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1"),
     libraryDependencies <++= (scalaVersion) { sv => Seq(
       sv.split("[.-]").toList match {
-        case "2" :: "8" :: _ => "org.scala-tools.testing" % "specs_2.8.1" % "1.6.8" % "test"
-        case "2" :: "9" :: "0" :: _ => "org.scala-tools.testing" % "specs_2.9.0-1" % "1.6.8" % "test"
+        case "2" :: "8" :: _ =>
+          "org.scala-tools.testing" % "specs_2.8.1" % "1.6.8" % "test"
+        case "2" :: "9" :: "0" :: _ =>
+          "org.scala-tools.testing" % "specs_2.9.0-1" % "1.6.8" % "test"
         case _ => "org.scala-tools.testing" %% "specs" % "1.6.9" % "test"
       })
     },
-    publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
+    publishTo := Some("Scala Tools Nexus" at
+      "http://nexus.scala-tools.org/content/repositories/releases/"),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
   )
   val httpShared = shared ++ Seq(
-    libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.1.2"
+    libraryDependencies +=
+      "org.apache.httpcomponents" % "httpclient" % "4.1.2"
   )
   lazy val dispatch =
     Project("Dispatch", file("."), settings = shared ++ Seq(
-      sources in (Compile, doc) <<= (thisProjectRef, buildStructure) flatMap aggregateTask(sources),
-      dependencyClasspath in (Compile, doc) <<= (thisProjectRef, buildStructure) flatMap aggregateTask(dependencyClasspath)
+      sources in (Compile, doc) <<=
+        (thisProjectRef, buildStructure) flatMap (aggregateTask(sources)),
+      dependencyClasspath in (Compile, doc) <<= 
+        (thisProjectRef, buildStructure) flatMap
+          aggregateTask(dependencyClasspath)
     )) aggregate(
       futures, core, http, nio, mime, json, http_json, oauth)
   lazy val futures =
@@ -59,12 +67,18 @@ object Dispatch extends Build {
     Project("dispatch-oauth", file("oauth"), settings = httpShared) dependsOn(
       core, http)
 
-  def aggregateTask[T](key: TaskKey[Seq[T]])(proj: ProjectRef, struct: Load.BuildStructure) = {
-    def collectProjects(op: ResolvedProject => Seq[ProjectRef])(projRef: ProjectRef, struct: Load.BuildStructure): Seq[ProjectRef] = {
+  def aggregateTask[T](key: TaskKey[Seq[T]])
+                      (proj: ProjectRef, struct: Load.BuildStructure) = {
+    def collectProjects(op: ResolvedProject => Seq[ProjectRef])
+                       (projRef: ProjectRef,
+                        struct: Load.BuildStructure): Seq[ProjectRef] = {
       val delg = Project.getProject(projRef, struct).toSeq.flatMap(op)
-      // Dependencies/aggregates might have their own dependencies/aggregates, so go recursive and do distinct.
+      // Dependencies/aggregates might have their own dependencies/aggregates
+      // so go recursive and do distinct.
       delg.flatMap(ref => ref +: collectProjects(op)(ref, struct)).distinct
     }
-    collectProjects(_.aggregate)(proj, struct).flatMap(key in (_, Compile, doc) get struct.data).join.map(_.flatten)
+    collectProjects(_.aggregate)(proj, struct).flatMap(
+      key in (_, Compile, doc) get struct.data
+    ).join.map(_.flatten)
   }
 }
