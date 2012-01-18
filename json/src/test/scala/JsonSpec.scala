10d7169df3e37999fd7c31df3f72b0c9b282cd18
Michael Ekstrand <michael@elehack.net>
2012-01-18 12:56:16 -0600

    Add some test cases for parsing and round-tripping JSON

--- json/src/test/scala/JsonSpec.scala
+++ json/src/test/scala/JsonSpec.scala
@@ -12,12 +12,59 @@ object JsValueSpec extends Specification {
     (block, System.currentTimeMillis - start)
   }
 
+  def reparse(x: Any) = Js(JsValue(x).toString)
+
+  "JS parsing" should {
+    "parse null" in {
+      Js("null") must be equalTo(JsNull)
+    }
+    "parse true" in {
+      Js("true") must be equalTo(JsTrue)
+    }
+    "parse false" in {
+      Js("false") must be equalTo(JsFalse)
+    }
+    "parse number" in {
+      Js("2394.3") must be equalTo(JsNumber(BigDecimal(2394.3)))
+    }
+    "parse string" in {
+      Js("\"foobie bletch\"") must be equalTo(JsString("foobie bletch"))
+    }
+    "parse escaped string" in {
+      Js("\"hello\\u203D\"") must be equalTo(JsString("hello\u203d"))
+    }
+    "parse array" in {
+      Js("[\"foo\", \"bar\"]") must be equalTo(JsArray(List(JsString("foo"), JsString("bar"))))
+    }
+    "parse object" in {
+      Js("{\"foo\": true}") must be equalTo(JsObject(Map(JsString("foo") -> JsTrue)))
+    }
+  }
+  
+  "JS round-tripping" should {
+    "round-trip null" in {
+      reparse(null) must be equalTo(JsNull)
+    }
+    "round-trip string" in {
+      reparse("HACKEM MUCHE") must be equalTo(JsString("HACKEM MUCHE"))
+    }
+    "round-trip array" in {
+      reparse(List("foo", false)) must be equalTo(JsArray(List(JsString("foo"), JsFalse)))
+    }
+    "round-trip object" in {
+      reparse(Map("scrolls" -> List("identify"), "name" -> "foo")) must be equalTo(
+        JsObject(Map(JsString("scrolls") -> JsArray(List(JsString("identify"))),
+                     JsString("name") -> JsString("foo"))))
+    }
+  }
+
   "JsValue.fromString and JsValue.fromStream" should {
+    val maxTime = 1000L
     "not become slow under alternate calling" in {
       val (_, t1) = time { JsValue.fromString(json) }
-      t1 must be_<=(1000L)  // normally finish within 1s
+      t1 must be_<=(maxTime)  // normally finish within 1s
       val (_, t2) = time { JsValue.fromStream(new ByteArrayInputStream(json.getBytes("utf-8"))) }
-      t2 must be_<=(1000L)
+      t2 must be_<=(maxTime)
     }
     
     "work properly under multi-thread calling" in {
@@ -32,7 +79,7 @@ object JsValueSpec extends Specification {
           })
         executor.execute(f)
         f
-      }.map{_.get}.toList mustNotExist {case (_, t) => t > 1000}
+      }.map{_.get}.toList mustNotExist {case (_, t) => t > maxTime}
     }
   }
 }
