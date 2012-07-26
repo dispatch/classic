ed50b35f00a2a1cd4ed9ee78c8698fe699495151
Adam Shannon <adam@ashannon.us>
2012-07-26 00:45:32 -0500

    It makes more sense to curry these together so you can just spwan off many requets from a single auth.

--- oauth/src/main/scala/foursquare.scala
+++ oauth/src/main/scala/foursquare.scala
@@ -4,8 +4,8 @@ import dispatch._
 object Auth {
   private val svc: Request = :/ ("api.foursquare.com") secure
 
-  def apply(version: String, creds: Pair[String, String], request: Request): Request = {
-    (svc / version) <& request <<? Map("client_id" -> creds._1, "client_secret" -> creds._2)
+  def apply(creds: Pair[String, String])(request: Request): Request = {
+    svc <& request <<? Map("client_id" -> creds._1, "client_secret" -> creds._2)
   }
 
 }
--- oauth/src/test/scala/foursquare.scala
+++ oauth/src/test/scala/foursquare.scala
@@ -6,7 +6,7 @@ import org.specs._
 object FoursquareSpec extends Specification {
   "A basic foursquare request that requires auth" should {
     "be able to be formed normally" in new context {
-      val builtReq = Auth("v2", ("key", "secret"), req).to_uri.toString
+      val builtReq = Auth(("key", "secret"))(req).to_uri.toString
 
       builtReq.contains("api.foursquare.com/v2/venues/search") must_== true
       builtReq.contains("client_id=key") must_== true
@@ -19,5 +19,5 @@ object FoursquareSpec extends Specification {
 }
 
 trait context {
-  val req = /\ / "venues" / "search" <<? Map("ll" -> "123.123,456.456", "v" -> "19700101")
+  val req = /\ / "v2" / "venues" / "search" <<? Map("ll" -> "123.123,456.456", "v" -> "19700101")
 }
