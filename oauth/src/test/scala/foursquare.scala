b5d3c6caac590fab99a2b8eec9419c2202ce3c8c
Adam Shannon <adam@ashannon.us>
2012-07-26 00:48:18 -0500

    And then really just make foursquare a bit easier to use.

--- oauth/src/test/scala/foursquare.scala
+++ oauth/src/test/scala/foursquare.scala
@@ -15,6 +15,17 @@ object FoursquareSpec extends Specification {
       builtReq.contains("v=19700101") must_== true
       builtReq.contains("https://") must_== true
     }
+
+    "be able to be formed normally through other apply" in new context {
+      val builtReq = Auth("key", "secret")(req).to_uri.toString
+
+      builtReq.contains("api.foursquare.com/v2/venues/search") must_== true
+      builtReq.contains("client_id=key") must_== true
+      builtReq.contains("client_secret=secret") must_== true
+      builtReq.contains("ll=123.123%2C456.456") must_== true
+      builtReq.contains("v=19700101") must_== true
+      builtReq.contains("https://") must_== true
+    }
   }
 }
 
